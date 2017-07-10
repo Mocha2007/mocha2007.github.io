d5094b6b9f859bc06a340c162fa039be3c57185a
mocha <mocha2007andcoco@gmail.com>
2017-07-10 15:46:47 -0400

    enhanced search

--- search.js
+++ search.js
@@ -1,3 +1,51 @@
 function doit(){
-document.write('<!DOCTYPE html><link href="simple.css" type="text/css" rel="stylesheet"><link rel="shortcut icon" href=molsico.png><title>MOLS Results for "'+document.getElementsByTagName("input")[0].value+'"</title>You searched for: "'+document.getElementsByTagName("input")[0].value+'".<br/><b>DICTIONARIES:</b><br/><a href="http://en.wiktionary.org/wiki/'+document.getElementsByTagName("input")[0].value+'#English">Wiktionary - English</a><br/><a href="http://ahdictionary.com/word/search.html?q='+document.getElementsByTagName("input")[0].value+'">American Heritage Dictionary</a><br/><a href="http://dictionary.cambridge.org/us/dictionary/english/'+document.getElementsByTagName("input")[0].value+'">Cambridge - English</a><br/><a href="http://collinsdictionary.com/dictionary/english/'+document.getElementsByTagName("input")[0].value+'">Collins - English</a><br/><a href="http://dictionary.com/browse/'+document.getElementsByTagName("input")[0].value+'">dictionary.com</a><br/><a href="http://ldoceonline.com/dictionary/'+document.getElementsByTagName("input")[0].value+'">Longman</a><br/><a href="http://macmillandictionary.com/search/british/?q='+document.getElementsByTagName("input")[0].value+'">Macmillan</a><br/><a href="http://merriam-webster.com/dictionary/'+document.getElementsByTagName("input")[0].value+'">Merriam-Webster</a><br/><a href="http://en.oxforddictionaries.com/definition/'+document.getElementsByTagName("input")[0].value+'">Oxford - English</a><br/><a href="http://oxfordlearnersdictionaries.com/us/definition/english/'+document.getElementsByTagName("input")[0].value+'_1">Oxford Advanced Learner\'s Dictionary</a><br/><a href="http://www.urbandictionary.com/define.php?term='+document.getElementsByTagName("input")[0].value+'">Urban Dictionary</a><br/><b>THESAURI:</b><br/><a href="http://en.wiktionary.org/wiki/Wikisaurus:'+document.getElementsByTagName("input")[0].value+'">Wikisaurus</a><br/><a href="http://collinsdictionary.com/dictionary/english-thesaurus/'+document.getElementsByTagName("input")[0].value+'">Collins</a><br/><a href="http://merriam-webster.com/thesaurus/'+document.getElementsByTagName("input")[0].value+'">Merriam-Webster</a><br/><a href="http://thesaurus.com/browse/'+document.getElementsByTagName("input")[0].value+'">thesaurus.com</a><br/><b>ENCYCLOPEDIAE:</b><br/><a href="http://en.wikipedia.org/wiki/'+document.getElementsByTagName("input")[0].value+'">Wikipedia</a><br/><b>GAME ENCYCLOPEDIAE:</b><br/><a href="http://dwarffortresswiki.org/index.php/DF2014:'+document.getElementsByTagName("input")[0].value+'">DFWiki</a><br/><a href="http://minecraft.gamepedia.com/'+document.getElementsByTagName("input")[0].value+'">MCWiki</a><br/><a href="http://terraria.gamepedia.com/'+document.getElementsByTagName("input")[0].value+'">Terraria Wiki</a><br/><img src="http://upload.wikimedia.org/wikipedia/commons/f/f5/Flag_of_Esperanto.svg" width="18" height="12"><b>RESURSOJ ESPERANTAJ:</b><br/><a href="http://vortaro.net/#'+document.getElementsByTagName("input")[0].value+'">vortaro.net</a><br/><a href="http://eo.wikipedia.org/wiki/'+document.getElementsByTagName("input")[0].value+'">Vikipedio</a><br/><a href="http://eo.wiktionary.org/wiki/'+document.getElementsByTagName("input")[0].value+'#Esperanto">Vikivortaro</a><br/><b>OTHER RESOURCES:</b><br/><a href="http://corpus.byu.edu/coca/">COCA</a><br/><a href="http://duckduckgo.com/?q='+document.getElementsByTagName("input")[0].value+'">DuckDuckGo</a><br/><a href="http://etymonline.com/index.php?search='+document.getElementsByTagName("input")[0].value+'">Etymonline</a><br/><a href="http://books.google.com/ngrams/graph?content='+document.getElementsByTagName("input")[0].value+'&year_start=1500&year_end=2008">Google Ngram</a><br/><a href="http://google.com/trends/explore?date=all&q='+document.getElementsByTagName("input")[0].value+'">Google Trends</a><br/><a href="http://www.perseus.tufts.edu/hopper/searchresults?q='+document.getElementsByTagName("input")[0].value+'">Perseus</a><br/><a href="http://reddit.com/search?q='+document.getElementsByTagName("input")[0].value+'">Reddit - Search</a><br/><a href="http://reddit.com/r/'+document.getElementsByTagName("input")[0].value+'">Reddit - Subreddit</a><br/><a href="http://reddit.com/u/'+document.getElementsByTagName("input")[0].value+'">Reddit - User</a><br/><a href="http://wolframalpha.com/input/?i='+document.getElementsByTagName("input")[0].value+'">Wolfram Alpha</a><br/><a href="http://youtube.com/results?search_query='+document.getElementsByTagName("input")[0].value+'">Youtube</a><br/>');
-}
+var s=document.getElementsByTagName("input")[0].value
+document.write(`<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<link href="simple.css" type="text/css" rel="stylesheet">
+<link href=dark.css type="text/css" rel="alternate stylesheet" title="Dark"> 
+<link href=blend.css type="text/css" rel="alternate stylesheet" title="Blend">
+<link href=print.css type="text/css" rel="stylesheet" media="print">
+<link rel="shortcut icon" href=molsico.png>
+<title>MOLS Results for "`+s+`"</title>
+You searched for: "`+s+`".<br/>
+<a href="search.html">Go Back</a><br/>
+<b>DICTIONARIES:</b><br/>
+<a href="http://en.wiktionary.org/wiki/`+s+`#English">Wiktionary - English</a><br/>
+<a href="http://ahdictionary.com/word/search.html?q=`+s+`">American Heritage Dictionary</a><br/>
+<a href="http://dictionary.cambridge.org/us/dictionary/english/`+s+`">Cambridge - English</a><br/>
+<a href="http://collinsdictionary.com/dictionary/english/`+s+`">Collins - English</a><br/>
+<a href="http://dictionary.com/browse/`+s+`">dictionary.com</a><br/>
+<a href="http://ldoceonline.com/dictionary/`+s+`">Longman</a><br/>
+<a href="http://macmillandictionary.com/search/british/?q=`+s+`">Macmillan</a><br/>
+<a href="http://merriam-webster.com/dictionary/`+s+`">Merriam-Webster</a><br/>
+<a href="http://en.oxforddictionaries.com/definition/`+s+`">Oxford - English</a><br/>
+<a href="http://oxfordlearnersdictionaries.com/us/definition/english/`+s+`_1">Oxford Advanced Learner\'s Dictionary</a><br/>
+<a href="http://www.urbandictionary.com/define.php?term=`+s+`">Urban Dictionary</a><br/>
+<b>THESAURI:</b><br/>
+<a href="http://en.wiktionary.org/wiki/Wikisaurus:`+s+`">Wikisaurus</a><br/>
+<a href="http://collinsdictionary.com/dictionary/english-thesaurus/`+s+`">Collins</a><br/>
+<a href="http://merriam-webster.com/thesaurus/`+s+`">Merriam-Webster</a><br/>
+<a href="http://thesaurus.com/browse/`+s+`">thesaurus.com</a><br/><b>ENCYCLOPEDIAE:</b><br/>
+<a href="http://en.wikipedia.org/wiki/`+s+`">Wikipedia</a><br/><b>GAME ENCYCLOPEDIAE:</b><br/>
+<a href="http://dwarffortresswiki.org/index.php/DF2014:`+s+`">DFWiki</a><br/>
+<a href="http://minecraft.gamepedia.com/`+s+`">MCWiki</a><br/>
+<a href="http://terraria.gamepedia.com/`+s+`">Terraria Wiki</a><br/>
+<img src="http://upload.wikimedia.org/wikipedia/commons/f/f5/Flag_of_Esperanto.svg" width="18" height="12"><b>RESURSOJ ESPERANTAJ:</b><br/>
+<a href="http://vortaro.net/#`+s+`">vortaro.net</a><br/>
+<a href="http://eo.wikipedia.org/wiki/`+s+`">Vikipedio</a><br/>
+<a href="http://eo.wiktionary.org/wiki/`+s+`#Esperanto">Vikivortaro</a><br/>
+<b>OTHER RESOURCES:</b><br/><a href="http://corpus.byu.edu/coca/">COCA</a><br/>
+<a href="http://duckduckgo.com/?q=`+s+`">DuckDuckGo</a><br/>
+<a href="http://etymonline.com/index.php?search=`+s+`">Etymonline</a><br/>
+<a href="http://books.google.com/ngrams/graph?content=`+s+`&year_start=1500&year_end=2008">Google Ngram</a><br/>
+<a href="http://google.com/trends/explore?date=all&q=`+s+`">Google Trends</a><br/>
+<a href="http://www.perseus.tufts.edu/hopper/searchresults?q=`+s+`">Perseus</a><br/>
+<a href="http://reddit.com/search?q=`+s+`">Reddit - Search</a><br/>
+<a href="http://reddit.com/r/`+s+`">Reddit - Subreddit</a><br/>
+<a href="http://reddit.com/u/`+s+`">Reddit - User</a><br/>
+<a href="http://wolframalpha.com/input/?i=`+s+`">Wolfram Alpha</a><br/>
+<a href="http://youtube.com/results?search_query=`+s+`">Youtube</a>
+</html>
+`);}
