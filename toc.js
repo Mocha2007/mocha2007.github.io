a03098eada7b1882a1fe79534a9972bbfa5146b5
mocha <mocha2007andcoco@gmail.com>
2017-12-27 16:39:06 -0500

    adapted toc.js for recommend.html

--- toc.js
+++ toc.js
@@ -1,6 +1,7 @@
 window.onload = function MakeTOC() {
 	//.getElementsByTagName('h1,h2,h3,h4,h5,h6')
-	namedheaders = document.querySelectorAll('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]');
+	namedheaders = document.querySelectorAll('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id],b.sub,b.super');
+
 	var text = `
 	<div class="desc">Navigation<br>
 	<a href="index.html">Home</a></div>
@@ -8,11 +9,23 @@ window.onload = function MakeTOC() {
 		<summary>On This Page</summary>`;
 
 	for (i=0;i<namedheaders.length;i++) {
-		var formattedname = namedheaders[i].id.replace(/_/g,' ');
-		var depth = namedheaders[i].tagName.slice(-1);
-		console.log(formattedname,depth);
+		if (namedheaders[i].tagName == 'B') {
+			namedheaders[i].id = namedheaders[i].innerHTML.replace(/\s/g,'_');/*Maybe I can just use this?!*/
+			var formattedname = namedheaders[i].innerHTML;
+
+			if (namedheaders[i].className == 'super') {
+				var depth = '2';
+			}
+			else {
+				var depth = '3';
+			}
+		}
+		else {
+			var formattedname = namedheaders[i].id.replace(/_/g,' ');
+			var depth = namedheaders[i].tagName.slice(-1);
+		}
 		text+='<span class="nav'+depth+'"><a href="#'+namedheaders[i].id+'">'+formattedname+'</a></span><br>';
-		//
+		console.log(formattedname,depth);
 	}
 	text+='</details>';
 	
