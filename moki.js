3c182a78bd64920721f0b1b6c8390b7cab30884c
mocha <mocha2007andcoco@gmail.com>
2018-01-18 16:59:52 -0500

    string exponent errors

--- moki.js
+++ moki.js
@@ -742,10 +742,15 @@ function fstep(){
 				}
 				// string
 				if (typeof a === 'string'){
-					if (b===0){
+					if (b<0 || b%1!==0){ // uh oh
+						console.error('bad string exponentiation\n@ Char '+line+'\n\t'+command);
+						mconsole('e','bad string exponentiation\n@ Char '+line+'\n\t'+command);
+						return true;
+					}
+					else if (b===0){ // anything^0 = 1 mostly
 						stack.push(1);
 					}
-					if (b===1){
+					else if (b===1){ // anything^1 = itself mostly
 						stack.push(a);
 					}
 					else {
