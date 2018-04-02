0b35c182720970499a281d5f1bbfa6e3fe755ac0
Mocha <mocha2007andcoco@gmail.com>
2018-04-01 22:23:11 -0400

    added JD and darian calendar

--- oneiatime.js
+++ oneiatime.js
@@ -6,6 +6,7 @@ var egyptseasons = ['Akhet','Peret','Shemu'];
 var chineseelements = ['Wood"><ruby>木<rt>き</rt></ruby>','Fire"><ruby>火<rt>ひ</rt></ruby>','Earth"><ruby>土<rt>つち</rt></ruby>','Metal"><ruby>金<rt>か</rt></ruby>','Water"><ruby>水<rt>みず</rt></ruby>'];
 var chinesesigns = ['Rat"><ruby>子<rt>ね</rt></ruby>','Ox"><ruby>丑<rt>うし</rt></ruby>','Tiger"><ruby>寅<rt>とら</rt></ruby>','Rabbit"><ruby>卯<rt>う</rt></ruby>','Dragon"><ruby>辰<rt>たつ</rt></ruby>','Snake"><ruby>巳<rt>み</rt></ruby>','Horse"><ruby>午<rt>うま</rt></ruby>','Goat"><ruby>未<rt>ひつじ</rt></ruby>','Monkey"><ruby>申<rt>さる</rt></ruby>','Rooster"><ruby>酉<rt>とり</rt></ruby>','Dog"><ruby>戌<rt>いぬ</rt></ruby>','Pig"><ruby>亥<rt>い</rt></ruby>'];
 var signs = ['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius','Pisces'];
+var marsmonths = ['Sagittarius','Dhanus','Capricornus','Makara','Aquarius','Kumbha','Pisces','Mina','Aries','Mesha','Taurus','Rishabha','Gemini','Mithuna','Cancer','Karka','Leo','Simha','Virgo','Kanya','Libra','Tula','Scorpius','Vrishika'];
 
 /* Thanks to https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers/17323608#17323608
 Fixes a bug in javascript where the entire fucking language is retarded and should burn in hell for all eternity.
@@ -75,6 +76,63 @@ function zodiac(){
 	"use strict";
 	var n = Math.floor(mod(new Date()-vernal,31556952000)/2629746000); // sign number 0-11
 	return signs[n];
+}
+
+function jd(){
+	"use strict";
+	return new Date()/86400000+2440587.5;
+}
+
+function dhelp(year){
+	"use strict";
+	return 668.591;
+	/*if (year<2001){
+		return (year-1)/2+year/10-year/100+year/1000;
+	}
+	if (year<4801){
+		return (year-1)/2+year/10-year/150;
+	}
+	if (year<6801){
+		return (year-1)/2+year/10-year/200;
+	}
+	if (year<8401){
+		return (year-1)/2+year/10-year/300;
+	}
+	return (year-1)/2+year/10-year/600;*/
+}
+
+function dhelp2(month){
+	"use strict";
+	return mod(month,6)===5?27:28;
+}
+
+function dhelp3(day){
+	"use strict";
+	var m = 0;
+	while (day>=dhelp2(m)){
+		day-=dhelp2(m);
+		m+=1;
+	}
+	return [m,Math.floor(day)];
+}
+
+function darian(){
+	"use strict";
+	var sol = 88775244; // Martian Sol; milliseconds
+	var n = (Number(new Date())+11392059672000)/sol; // sols past epoch
+	var tn = n;
+	var marsy = 0;
+	while (true){
+		if (tn<dhelp(marsy+1)){
+			var marsd = tn;
+			break;
+		}
+		tn-=dhelp(marsy+1);
+		marsy+=1;
+	}
+	var marsmonth = marsmonths[dhelp3(marsd)[0]];
+	var marsday = dhelp3(marsd)[1];
+	return [marsday,marsmonth,marsy].join(' ');
 }
 
 function HolidayCSS(){
@@ -158,5 +216,5 @@ function OneiaTime(){
 
 function bonus(){
 	"use strict";
-	document.getElementById("clockbonus").innerHTML = zodiac()+'<br/>'+china()+'<br/>'+egypt()+'<br/>'+maya();
+	document.getElementById("clockbonus").innerHTML = zodiac()+'<br/>'+china()+'<br/>'+egypt()+'<br/>'+maya()+'<br/>JD '+jd().toFixed(3)+'<br/>'+darian();
 }
