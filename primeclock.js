4da80d8c2290683892796b412d93eee624b2c1e4
Mocha <mocha2007andcoco@gmail.com>
2018-04-02 16:24:55 -0400

    more future events

--- primeclock.js
+++ primeclock.js
@@ -1,6 +1,7 @@
 var i;
 var year = 31556952;
-var events = [ // MUST BE REVERSE CHRONO ORDER!!!
+var yy = 365.2425;
+var events = [ // MUST BE REVERSE CHRONO ORDER!!! time before 01 jan 2018
 [14e9,'<a href="https://en.wikipedia.org/wiki/Big_Bang">Big Bang</a>'],
 [12.7e9,'Formation of the Milky Way'],
 [4.6e9,'Formation of the Solar System'],
@@ -9,35 +10,45 @@ var events = [ // MUST BE REVERSE CHRONO ORDER!!!
 [3.465e9,'Life forms on Earth'],
 [3.2e9,'Earliest Photosynthesis'],
 [2.4e9,'Huronian Glaciation'],
+[1.7e9,'First <a href="https://en.wikipedia.org/wiki/Mitochondrion">Mitochondia</a>'],
 [1.6e9,'First Multicellular Life on Earth'],
 [1.2e9,'First Sexual Reproduction'],
 [541e6,'Cambrian Explosion'],
 [485.4e6,'Cambrian-Ordovician Extinction Event'],
 [430e6,'Final Ordovician–Silurian Extinction Event'],
+[428e6,'Earliest Land Animals'],
 [367.5e6,'Late Devonian Extinction Event'],
 [335e6,'Pangaea Forms'],
 [252e6,'Permian-Triassic Extinction Event'],
+[231.4e6,'Earliest <a href="https://en.wikipedia.org/wiki/Dinosaur">Dinosaurs</a>'],
 [225e6,'Earliest <a href="https://en.wikipedia.org/wiki/Mammal">Mammals</a>'],
 [201.3e6,'Triassic-Jurassic Extinction Event'],
 [175e6,'Pangaea Separates'],
 [145e6,'Beginning of the Cretaceous Period'],
 [140e6,'Beginning of the <a href="https://en.wikipedia.org/wiki/Sevier_orogeny">Sevier Orogeny</a>'],
 [125e6,'Beginning of the <a href="https://en.wikipedia.org/wiki/Cretaceous_Terrestrial_Revolution">Cretaceous Terrestrial Revolution</a>'],
+[120e6,'Formation of the <a href="https://en.wikipedia.org/wiki/BP_Structure">BP Structure</a>'],
+[115e6,'<a href="https://en.wikipedia.org/wiki/Carswell_crater">Carswell Impact</a>'],
 [94e6,'Cenomanian-Turonian Boundary Event'],
 [90e6,'Beginning of the <a href="https://en.wikipedia.org/wiki/Andean_orogeny">Andean Orogeny</a>'],
 [80e6,'Beginning of the <a href="https://en.wikipedia.org/wiki/Laramide_orogeny">Laramide Orogeny</a>'],
+[68e6,'Earliest <a href="https://en.wikipedia.org/wiki/Tyrannosaurus">Tyrannosaurs</a>'],
 [66e6,'Cretaceous-Tertiary Extinction Event'],
+[65.17e6,'<a href="https://en.wikipedia.org/wiki/Boltysh_crater">Boltysh Impact</a>'],
 [42e6,'Earliest <i><a href="https://en.wikipedia.org/wiki/Carnivora">Carnivoran</a></i> Fossils'],
 [33.9e6,'Eocene-Oligocene Extinction Event'],
 [14.5e6,'Middle Miocene Disruption'],
 [7e6,'Earliest <i><a href="https://en.wikipedia.org/wiki/Vulpes">Vupes</a></i> Fossils'],
+[3330,'<a href="https://en.wikipedia.org/wiki/Mursili\'s_eclipse">Mursili\'s Eclipse'],
 [242,'Formation of the United States'],
 [104,'Beginning of the Great War'],
 [79,'Beginning of the Second World War'],
 [68,'Beginning of the Korean War'],
 [63,'Beginning of the Vietnam War'],
-[17,'September 11 Attacks'],
-[1,'Trump Elected!']
+// Precise number of days for events before 2000 unnecessary.
+[5956/yy,'<a href="https://en.wikipedia.org/wiki/September_11_attacks">September 11 Attacks</a>'],
+[5949/yy,'<a href="https://en.wikipedia.org/wiki/2001_anthrax_attacks">2001 Anthrax Attacks</a>']
+[419/yy,'Trump Elected!']
 ];
 
 function factorize(n){
