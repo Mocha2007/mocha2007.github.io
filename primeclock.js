53441778b904020ca8a8a9746457d33f974a0273
Mocha <mocha2007andcoco@gmail.com>
2018-04-02 22:23:31 -0400

    added more new events

--- primeclock.js
+++ primeclock.js
@@ -3,6 +3,7 @@ var i;
 var currentyear = ((new Date()).getFullYear());
 var year = 31557600;//31556952; will be irrelevant to this program until 2100
 var yy = 365.2425;
+var debug = false; // enable to see all events at any time
 
 function toYear(ms){
 	"use strict";
@@ -99,21 +100,49 @@ var events = [ // MUST BE REVERSE CHRONO ORDER!!! time before 01 jan 2018
 [56e6,'Beginning of the <a href="https://en.wikipedia.org/wiki/Eocene">Eocene</a>'],
 [55e6,'Earliest <a href="https://en.wikipedia.org/wiki/Primate">Primates</a>'],
 [52e6,'Earliest <a href="https://en.wikipedia.org/wiki/Bat">Bats</a>'],
+[45e6,'Australia Splits from Antarctica'],
 [42e6,'Earliest <a href="https://en.wikipedia.org/wiki/Carnivora">Carnivorans</a>'],
 [33.9e6,'Eocene-Oligocene Extinction Event'],
+[23.03e6,'Beginning of the <a href="https://en.wikipedia.org/wiki/Neogene">Neogene</a>'],
 [14.5e6,'Middle Miocene Disruption'],
-[7e6,'Earliest <i><a href="https://en.wikipedia.org/wiki/Vulpes">Vupes</a></i> Fossils'],
+[7e6,'Earliest <i><a href="https://en.wikipedia.org/wiki/Vulpes">Vulpes</a></i> Fossils'],
+[2.588e6,'Beginning of the <a href="https://en.wikipedia.org/wiki/Quaternary">Quaternary</a>'],
+[315e3,'Modern Humans Evolve'],
+[110e3,'<a href="https://en.wikipedia.org/wiki/Last_glacial_period">Last Glacial Period</a> Begins'],
+[11.7e3,'<a href="https://en.wikipedia.org/wiki/Last_glacial_period">Last Glacial Period</a> Ends'],
 // Above events should be forever static
-[currentyear+1312,'<a href="https://en.wikipedia.org/wiki/Mursili\'s_eclipse">Mursili\'s Eclipse'],
+[currentyear+4500,'Dawn of <a href="https://en.wikipedia.org/wiki/Sumer">Sumer</a>'],
+[currentyear+3500,'Approximate age of <a href="https://en.wikipedia.org/wiki/Proto-Indo-European_language">Proto-Indo-European</a>'],
+[currentyear+1312,'<a href="https://en.wikipedia.org/wiki/Mursili\'s_eclipse">Mursili\'s Eclipse</a>'],
+[currentyear+753,'<a href="https://en.wikipedia.org/wiki/Ab_urbe_condita">Founding of Rome</a>'],
+[currentyear+218,'Hannibal <a href="https://en.wikipedia.org/wiki/Hannibal\'s_crossing_of_the_Alps">Crosses the Alps</a>'],
+[currentyear+44,'<a href="https://en.wikipedia.org/wiki/Assassination_of_Julius_Caesar">Assassination of Julius Caesar</a>'],
+[currentyear-476,'<a href="https://en.wikipedia.org/wiki/Fall_of_the_Western_Roman_Empire">Fall of West Rome</a>; Dawn of the <a href="https://en.wikipedia.org/wiki/Middle_Ages">Middle Ages</a>'],
+[currentyear-1453,'Fall of <a href="https://en.wikipedia.org/wiki/Byzantine_Empire">Byzantium</a>'],
+[currentyear-1715,'Beginning of the <a href="https://en.wikipedia.org/wiki/Age_of_Enlightenment">Age of Enlightenment</a>'],
 [currentyear-1776,'Formation of the United States'],
+[currentyear-1812,'Beginning of the <a href="https://en.wikipedia.org/wiki/War_of_1812">War of 1812</a>'],
+[currentyear-1846,'Beginning of the <a href="https://en.wikipedia.org/wiki/Mexican–American_War">Mexican-American War</a>'],
+[currentyear-1861,'Beginning of the <a href="https://en.wikipedia.org/wiki/American_Civil_War">American Civil War</a>'],
+[currentyear-1898,'Beginning of the <a href="https://en.wikipedia.org/wiki/Spanish–American_War">Spanish-American War</a>'],
 [currentyear-1914,'Beginning of the Great War'],
 [currentyear-1939,'Beginning of the Second World War'],
 [currentyear-1950,'Beginning of the Korean War'],
 [currentyear-1955,'Beginning of the Vietnam War'],
-// Precise number of days for events before 2000 unnecessary.
+// Precise number of days for events before 1970 unnecessary.
+[diff(495432000000),'<a href="https://en.wikipedia.org/wiki/Super_Mario_Bros.">Super Mario Bros.</a> Released'],
+[diff(682056000000),'<a href="https://en.wikipedia.org/wiki/Super_Mario_World">Super Mario World</a> Released in NA'],
 [diff(1000212360000),'<a href="https://en.wikipedia.org/wiki/September_11_attacks">September 11 Attacks</a>'],
 [diff(1000771200000),'<a href="https://en.wikipedia.org/wiki/2001_anthrax_attacks">2001 Anthrax Attacks</a>'],
-[diff(1478667600000),'Donald Trump Elected President of the US'] // roughly when it became certain; 00:00 EST 9 NOV 2016 
+[diff(1059710400000),'<a href="https://en.wikipedia.org/wiki/Myspace">Myspace</a> Launched'],
+[diff(1075870800000),'<a href="https://en.wikipedia.org/wiki/Facebook">Facebook</a> Launched'],
+[diff(1108357200000),'<a href="https://en.wikipedia.org/wiki/YouTube">YouTube</a> Launched'],
+[diff(1242532800000),'<a href="https://en.wikipedia.org/wiki/Minecraft">Minecraft Classic</a> Released'],
+[diff(1305172800000),'<a href="https://en.wikipedia.org/wiki/Death_of_Osama_bin_Laden">Death of Osama bin Laden</a>'],
+[diff(1307332800000),'<a href="https://en.wikipedia.org/wiki/Twitch.tv">Twitch</a> Launched'],
+[diff(1359003600000),'<a href="https://en.wikipedia.org/wiki/Vine_(service)">Vine</a> Launched'],
+[diff(1478667600000),'Donald Trump Elected President of the US'], // roughly when it became certain; 00:00 EST 9 NOV 2016 
+[diff(1494907200000),'<a href="https://mocha2007.github.io">Mocha\'s Site</a> Launched']
 // https://en.wikipedia.org/wiki/Timelines_of_world_history
 ];
 
@@ -241,7 +270,7 @@ function alc(){
 	var y = Math.pow(a,1-x/year);
 	var str = '';
 	for (i=0;i<events.length;i+=1){
-		if (events[i][0]>y){
+		if (debug || events[i][0]>y){
 			str+='<br>'+(i===0?'Jan 01 '+currentyear+' 00:00:00':ialc(events[i][0]))+' - '+events[i][1];
 		}
 		else {
