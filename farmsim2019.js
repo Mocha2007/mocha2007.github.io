07884ea0623ea39c2a34dabb84041d54ef86cdcb
Mocha <mocha2007andcoco@gmail.com>
2018-04-04 14:33:10 -0400

    now it's semi-playable...

--- farmsim2019.js
+++ farmsim2019.js
@@ -1,27 +1,49 @@
-fps = 20;
-clock = 0;
-score = 0;
-health = 100;
-paused = false;
+var fps = 20;
+var clock = 0;
+var score = 0;
+var health = 100;
+var paused = false;
+
+var llamahp = 10;
+
+var timeElement = document.getElementById("time");
+var scoreElement = document.getElementById("score");
+var healthElement = document.getElementById("health");
+var enemyhealthElement = document.getElementById("hp");
+var llamaElement = document.getElementById("llama");
+
+function click(x){
+	"use strict";
+	if (x==='llama'){
+		llamahp -= 1;
+		console.log('LLAMA SHOT');
+	}
+}
 
 function main(){
+	"use strict";
 	if (!paused){
 		clock+=1;
+		// make cones
+		
+		if (llamahp===0){
+			llamaElement.classList = ["invisible"];
+			score += Math.floor(fps*1000/clock);
+			llamahp -= 1;
+		}
 
 		// ONLY update if absolutely necessary!
-
-		var timeElement = document.getElementById("time");
-		var scoreElement = document.getElementById("score");
-		var healthElement = document.getElementById("health");
-		
-		if (timeElement.innerHTML !== String((clock/fps).toFixed(1))){
-			timeElement.innerHTML = (clock/fps).toFixed(1);
+		if (llamahp>0 && timeElement.innerHTML !== String(Math.floor(clock/fps))){
+			timeElement.innerHTML = clock/fps;
 		}
 		if (scoreElement.innerHTML !== String(score)){
 			scoreElement.innerHTML = score;
 		}
 		if (healthElement.innerHTML !== String(health)){
 			healthElement.innerHTML = health;
+		}
+		if (enemyhealthElement.innerHTML !== String(Math.max(0,llamahp))){
+			enemyhealthElement.innerHTML = Math.max(0,llamahp);
 		}
 	}
 }
