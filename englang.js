1546d6caf6f7c99403446f798efafd49a78bf8ff
Mocha <mocha2007andcoco@gmail.com>
2018-06-29 13:45:12 -0400

    conlang section update

---
+++ englang.js
@@ -0,0 +1,30 @@
+var stack,valency,x;
+
+function reset(){
+	stack = [];
+	return false;
+}
+
+function parse(){
+	reset();
+	var input = document.getElementById('text').value.split(' ');
+	input.reverse();
+	while (input.length){
+		x = input.pop();
+		if (x !== ''){
+			valency = 0;
+			while (true){
+				if (input[input.length-1] === 'ba'){
+					input.pop();
+					valency += 1;
+				}
+				else {
+					break;
+				}
+			}
+			stack.push('<span class="v'+valency+'">'+x+'</span>');
+		}
+	}
+	document.getElementById('translation').innerHTML = stack.join(' ');
+	return false;
+}
