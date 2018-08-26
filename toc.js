9c7a7beb29c601b416d9e3c94072a5a7c37f4701
Mocha <mocha2007andcoco@gmail.com>
2018-08-26 14:57:52 -0400

    better toc

--- toc.js
+++ toc.js
@@ -1,4 +1,4 @@
-function MainMakeTOC() {
+function MakeTOC() {
 	"use strict";
 	//.getElementsByTagName('h1,h2,h3,h4,h5,h6')
 	var namedheaders = document.querySelectorAll('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id],b.sub,b.super');
@@ -30,7 +30,4 @@ function MainMakeTOC() {
 	document.getElementById("nav").innerHTML = text;
 };
 
-window.onload = function MakeTOC() {
-	"use strict";
-	MainMakeTOC();
-};
+window.onload = MakeTOC;
