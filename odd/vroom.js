3c0f504b699b02a4dcb3de0a49d08f6c27568b61
Mocha <mocha2007andcoco@gmail.com>
2018-09-18 19:39:28 -0400

    more cleanup

--- odd/vroom.js
+++ odd/vroom.js
@@ -3,37 +3,46 @@ function play(name){
 	(new Audio('https://www.myinstants.com/media/sounds/'+name+'.mp3')).play();
 }
 
+function settilde(string){
+    "use strict";
+	document.getElementById('tilde').innerHTML = string;
+}
+
+function tildesize(){
+    "use strict";
+	var x = window.getComputedStyle(document.querySelector('#tilde')).fontSize;
+	return parseFloat(x.slice(0,-2));
+}
+
+function setsize(x){
+    "use strict";
+	document.getElementById("tilde").style["font-size"] = String(x) + 'px';
+}
+
 function tilde(){
     "use strict";
 	var i;
 	var ratio = 0.99; // must be .99 < r < 1
 	var duration = 600;
+	var defaultsize = tildesize();
 	// audio
 	play('yandere-dev-woah-technology');
 	// visual
-	document.getElementById('tilde').innerHTML = "❄️";
+	settilde("❄️");
 	// initialize
-	var currentsize = window.getComputedStyle(document.querySelector('#tilde')).fontSize;
-	var defaultsize = currentsize;
-	currentsize = parseFloat(currentsize.slice(0,-2));
-	var newsize = currentsize * 0.62; // minimize jumping
-	newsize = String(newsize) + 'px';
-	document.getElementById("tilde").style["font-size"] = newsize;
+	var currentsize = defaultsize;
+	setsize(currentsize * 0.62); // minimize jumping
 	// main
 	for (i = 0; i < duration; i += 1 ){
 		// https://stackoverflow.com/a/16873849/2579798
 		setTimeout(function(){
-			currentsize = window.getComputedStyle(document.querySelector('#tilde')).fontSize;
-			currentsize = parseFloat(currentsize.slice(0,-2));
-			newsize = currentsize * ratio;
-			// console.log(currentsize,"->",newsize);
-			newsize = String(newsize) + 'px';
-			document.getElementById("tilde").style["font-size"] = newsize;
+			currentsize = tildesize();
+			setsize(currentsize * ratio);
 		}, i*4);
 	}
 	// reset
 	setTimeout(function(){
-		document.getElementById('tilde').innerHTML = "~️";
-		document.getElementById("tilde").style["font-size"] = defaultsize;
+		settilde("~️");
+		setsize(defaultsize);
 	}, duration*4);
 }
