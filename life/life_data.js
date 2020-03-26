74580e546ef865450eae6c418e1754acd0bb3b88
Mocha <mocha2007andcoco@gmail.com>
2020-03-26 10:55:18 -0400

    css

--- life/life_data.js
+++ life/life_data.js
@@ -1,18 +1,44 @@
 var life_data = [
 	{
+		'name': 'animalia',
+		'rank': 'kingdom',
+		'parent': 'life',
+	},
+	{
+		'name': 'chordata',
+		'rank': 'phylum',
+		'parent': 'animalia',
+		'desc': 'During some period of their life cycle, chordates possess a notochord, a dorsal nerve cord, pharyngeal slits, an endostyle, and a post-anal tail: these five anatomical features define this phylum.'
+	},
+	{
+		'name': 'hominidae',
+		'rank': 'family',
+		'parent': 'primates',
+	},
+	{
+		'name': 'homo',
+		'rank': 'genus',
+		'parent': 'hominidae',
+	},
+	{
+		'name': 'homo sapiens',
+		'rank': 'species',
+		'parent': 'homo',
+		'desc': 'you',
+	},
+	{
 		'name': 'life',
 		'rank': 'life',
 		'parent': '*',
 	},
 	{
-		'name': 'animal',
-		'rank': 'kingdom',
-		'parent': 'life',
+		'name': 'mammalia',
+		'rank': 'class',
+		'parent': 'chordata',
 	},
 	{
-		'name': 'human',
-		'rank': 'species',
-		'parent': 'animal',
-		'desc': 'you',
+		'name': 'primates',
+		'rank': 'order',
+		'parent': 'mammalia',
 	},
 ];
