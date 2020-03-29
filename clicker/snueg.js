637880c13c59d32766c7bc49ae23056e82c5c780
Mocha <mocha2007andcoco@gmail.com>
2020-03-29 10:52:12 -0400

    snueg

---
+++ clicker/snueg.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6, strict: true, strict: global */
+/* globals ages, life_data */
+/* exported open_age, search_button, toggle, main */
+"use strict";
+
+var game = {};
+
+function main(){
+	/*
+	// set up ticks
+	updateFPS();
+	setInterval(redrawInterface, 1000);
+	setInterval(gameTick, 1000/Game.settings.fps);
+	setInterval(redrawMap, 1000/Game.settings.fps);
+	*/
+}
