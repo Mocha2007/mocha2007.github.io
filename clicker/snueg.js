bb7d097df80adf1d82c9bfceae7598a28ae17d67
Mocha <mocha2007andcoco@gmail.com>
2020-03-31 11:50:42 -0400

    version # fix

--- clicker/snueg.js
+++ clicker/snueg.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 6, strict: true, strict: global, eqeqeq: true */
 /* exported delete_cookie, downloadSave, guide, importSave, main, prestige, snuegButton */
 "use strict";
-var version = "a200330";
+var version = "a200331";
 
 // classes
 
