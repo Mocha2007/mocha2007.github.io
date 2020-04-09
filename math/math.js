6df66bb0b3f00ba3a7af441d16e6cba09fa3d1a7
Mocha <mocha2007andcoco@gmail.com>
2020-04-09 15:42:58 -0400

    sub product

--- math/math.js
+++ math/math.js
@@ -14,6 +14,9 @@ class Complex {
 	get argument(){
 		return Math.atan2(this.imag, this.real);
 	}
+	get conjugate(){
+		return new Complex(this.real, -this.imag);
+	}
 	get magnitude(){
 		return Math.sqrt(this.real * this.real + this.imag * this.imag);
 	}
@@ -23,7 +26,22 @@ class Complex {
 		if (other instanceof Complex){
 			return new Complex(this.real + other.real, this.imag + other.imag);
 		}
-		return new Complex(this.real + other, this.imag);
+		return this.add(new Complex(other));
+	}
+	/** @param {number|Complex} other */
+	sub(other){
+		if (other instanceof Complex){
+			return this.add(other.product(-1));
+		}
+		return this.sub(new Complex(other));
+	}
+	/** @param {number|Complex} other */
+	product(other){
+		if (other instanceof Complex){
+			return new Complex(this.real * other.real - this.imag * other.imag,
+				this.real * other.imag + this.imag * other.real);
+		}
+		return new Complex(this.real*other, this.imag*other);
 	}
 	// constants
 	static get i(){
