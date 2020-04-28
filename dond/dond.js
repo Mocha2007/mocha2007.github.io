c20b6482df484d2e83452df82f78eefd00de4350
Mocha <mocha2007andcoco@gmail.com>
2020-04-27 23:00:40 -0400

    DONE!

--- dond/dond.js
+++ dond/dond.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 6, strict: true, strict: global */
-/* globals commaNumber, random */
+/* globals commaNumber, mean, random, round */
 'use strict';
 
 const caseValues = [
@@ -38,13 +38,15 @@ class Case {
 		if (!Game.chosen){
 			Game.chosen = this.num;
 			document.getElementById(this.id).classList.add('chosen');
-			Game.log('You have selected case ' + this.num + '. Please choose another to open...');
-			return;
+			Game.log('You have selected case ' + this.num + '.');
 		}
 		// otherwise, the player is choosing it...
-		this.open();
-		this.value.reveal();
-		Game.log('The case was worth $' + commaNumber(this.value.value) + '! Please choose another to open...');
+		else {
+			this.open();
+			this.value.reveal();
+			Game.log('The case was worth $' + commaNumber(this.value.value) + '!');
+		}
+		Game.banker.update();
 
 	}
 	open(){
@@ -85,6 +87,44 @@ class Value {
 }
 
 const Game = {
+	banker: {
+		callsAt: [20, 15, 11, 8, 6, 5, 4, 3, 2],
+		/** ie., the number of previous times the banker has called */
+		get callId(){
+			const remaining = Game.casesUnopened.length;
+			return this.callsAt.indexOf(this.callsAt.filter(x => x <= remaining)[0]);
+		},
+		callWeights: [0.1, 0.3, 0.75, 0.83, 0.95, 1, 1, 1, 1],
+		no(){
+			Game.log('You have declined the banker\'s offer! Please select another case...');
+		},
+		get offer(){
+			return round(mean(Game.casesUnopened.map(c => c.value.value)) *
+				this.callWeights[this.callId]);
+		},
+		get timeUntilNextCall(){
+			const nextCallId = this.callId;
+			return Game.casesUnopened.length - this.callsAt[nextCallId];
+		},
+		update(){
+			const tunc = this.timeUntilNextCall;
+			if (tunc){
+				document.getElementById('casesUntilNextCall').innerHTML = 'Please select ' + this.timeUntilNextCall + ' more cases...';
+				return;
+			}
+			document.getElementById('casesUntilNextCall').innerHTML = '';
+			Game.log('The banker offers you $' + commaNumber(this.offer) + `. Do you accept?<br>
+			<a href="javascript:Game.banker.yes()">YES</a>
+			<a href="javascript:Game.banker.no()">NO</a>`);
+		},
+		yes(){
+			const moolah = Game.cases[Game.chosen].value.value;
+			Game.log('You take the $' + commaNumber(this.offer) +
+			'! Howie opens your case, and inside was $' + commaNumber(moolah) + '! ' +
+			(moolah <= this.offer ? 'A wise choice!' : 'An unfortunate decision!') +
+			' <a href="Game.new()">Play Again?</a>');
+		},
+	},
 	build(){
 		// build cases and values
 		caseValues.forEach((_, i) => {
@@ -98,6 +138,9 @@ const Game = {
 	},
 	/** @type {Case[]} */
 	cases: [],
+	get casesUnopened(){
+		return Game.cases.filter(c => !c.opened);
+	},
 	/** the ID of the case, not the index. so the min value is 1 */
 	chosen: 0,
 	log(s = ''){
