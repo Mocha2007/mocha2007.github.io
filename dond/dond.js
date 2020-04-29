db571fc555fd8b50ca44196d8bc916e27b813329
Mocha <mocha2007andcoco@gmail.com>
2020-04-28 23:45:43 -0400

    ugh fml

--- dond/dond.js
+++ dond/dond.js
@@ -284,6 +284,57 @@ document.addEventListener('click', clickFunction);
 // https://bitbucket.org/newgrounds/newgrounds.io-for-javascript-html5/src/default/
 // eslint-disable-next-line new-cap
 const ngio = new Newgrounds.io.core('50305:QVq4Lget', '4IsS3BhJEGFLgXhptkZTpKWmtWH3guHL');
+
+function onLoggedIn(){
+	console.log('Welcome ' + ngio.user.name + '!');
+}
+
+function onLoginFailed(){
+	console.log('There was a problem logging in: ' . ngio.login_error.message);
+}
+
+function onLoginCancelled(){
+	console.log('The user cancelled the login.');
+}
+
+/*
+ * Before we do anything, we need to get a valid Passport session.  If the player
+ * has previously logged in and selected 'remember me', we may have a valid session
+ * already saved locally.
+ */
+function initSession(){
+	ngio.getValidSession(function(){
+		if (ngio.user){
+			/*
+             * If we have a saved session, and it has not expired,
+             * we will also have a user object we can access.
+             * We can go ahead and run our onLoggedIn handler here.
+             */
+			onLoggedIn();
+		}
+		else {
+			/*
+             * If we didn't have a saved session, or it has expired
+             * we should have been given a new one at this point.
+             * This is where you would draw a 'sign in' button and
+             * have it execute the following requestLogin function.
+             */
+			requestLogin();
+		}
+	});
+}
+
+/*
+ * Call this when the user clicks a 'sign in' button from your game.  It MUST be called from
+ * a mouse-click event or pop-up blockers will prevent the Newgrounds Passport page from loading.
+ */
+function requestLogin(){
+	ngio.requestLogin(onLoggedIn, onLoginFailed, onLoginCancelled);
+	/* you should also draw a 'cancel login' buton here */
+}
+
+initSession();
+
 /* vars to record any medals and scoreboards that get loaded */
 let medals; // todo scoreboards;
 
@@ -311,22 +362,18 @@ function onMedalUnlocked(medal){
 function unlockMedal(medalName){
 	/* If there is no user attached to our ngio object, it means the user isn't logged in and we can't unlock anything */
 	if (!ngio.user) return;
-
-	let medal;
-
 	for (let i = 0; i < medals.length; i++){
-		medal = medal[i];
-
+		const medal = medals[i];
 		/* look for a matching medal name */
 		if (medal.name === medalName){
 			/* we can skip unlocking a medal that's already been earned */
 			if (!medal.unlocked){
 				/* unlock the medal from the server */
+				console.log('Attempting to unlock', medal);
 				ngio.callComponent('Medal.unlock', {id: medal.id}, function(result){
 					if (result.success) onMedalUnlocked(result.medal);
 				});
 			}
-
 			return;
 		}
 	}
