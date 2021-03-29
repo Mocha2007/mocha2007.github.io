21d20198f7f18a468b2b436e46957c9be36f1f1e
mocha <3129210+Mocha2007@users.noreply.github.com>
2021-03-28 22:24:25 -0400

    better spacing

--- maps/galaxy.js
+++ maps/galaxy.js
@@ -115,11 +115,13 @@ class Body {
 		tt.innerHTML = '<center><b>' + this.name + '</b></center>' + proper(this.type);
 		if (this.img)
 			tt.innerHTML += '<center><img src="'+this.img+'" height="100px"></center>';
+		else
+			tt.innerHTML += '<br>';
 		tt.innerHTML += `RA: ${round(this.coords.ra/pi*180, 2)}&deg;<br>
 		DEC: ${round(this.coords.dec/pi*180, 2)}&deg;<br>
 		DIST: ${round(this.dist/ly)} ly`;
 		if (this.desc)
-			tt.innerHTML += this.desc;
+			tt.innerHTML += '<br>' + this.desc;
 		return tt;
 	}
 	createElement(){
