0e6cb4367a39f4388501bbe4d32bc0bce218ae75
mocha <3129210+Mocha2007@users.noreply.github.com>
2021-09-25 12:19:53 -0400

    more complex formatting test

--- namei/eredict.js
+++ namei/eredict.js
@@ -1,8 +1,9 @@
 /* exported getEreDict */
 'use strict';
-
 // shamelessly stolen from verdurian dict,
 // didn't know this was even possible so I'm hyped to get this working!
+
+// eslint-disable-next-line no-undef
 const reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
 function getEreDict(){
 	reader.open('get', 'eremoran.txt', true);
@@ -11,14 +12,30 @@ function getEreDict(){
 }
 
 function printEreDict(){
+	// this is required to prevent it from double-posting
 	if (reader.readyState !== 4)
 		return;
 	/** @type {HTMLDListElement} */
 	const list = document.getElementById('dictlist');
 	reader.responseText.split('\n').forEach(line => {
-		// todo!
+		const [index, type, defs] = line.split('=');
+		// entry title
 		const dt = document.createElement('dt');
-		dt.innerHTML = line;
+		dt.innerHTML = index;
+		// type, eg. noun, verb, ...
+		const typeelement = document.createElement('dd');
+		typeelement.innerHTML = type;
+		// def(s)
+		const entryelement = document.createElement('dd');
+		const deflist = document.createElement('ol');
+		entryelement.appendChild(deflist);
+		defs.split(';').forEach(def => {
+			const d = document.createElement('li');
+			d.innerHTML = def;
+			deflist.appendChild(d);
+		});
 		list.appendChild(dt);
+		list.appendChild(typeelement);
+		list.appendChild(entryelement);
 	});
 }
