81db156d7a1c854de6a1307585523903d4a14b72
Mocha <3129210+Mocha2007@users.noreply.github.com>
2021-12-14 11:54:22 -0500

    more phones

--- lang/clongData.js
+++ lang/clongData.js
@@ -1,5 +1,90 @@
+/* exported phones */
 'use strict';
 const phones = [
+	{
+		name: 'a',
+		properties: {
+			isVowel: true,
+			freq: 2600/3183,
+		},
+	},
+	{
+		name: 'b',
+		properties: {
+			manner: 'plosive',
+			place: 'bilabial',
+			voiced: true,
+			freq: 1906/3183,
+		},
+	},
+	{
+		name: 'c',
+		properties: {
+			manner: 'plosive',
+			place: 'palatal',
+			voiced: false,
+			freq: 418/3183,
+		},
+	},
+	{
+		name: 'd',
+		properties: {
+			manner: 'plosive',
+			place: 'alveolar',
+			voiced: true,
+			freq: 1376/3183,
+		},
+	},
+	{
+		name: 'e',
+		properties: {
+			isVowel: true,
+			freq: 1841/3183,
+		},
+	},
+	{
+		name: 'f',
+		properties: {
+			manner: 'fricative',
+			place: 'labiodental',
+			voiced: false,
+			freq: 1329/3183,
+		},
+	},
+	{
+		name: 'g',
+		properties: {
+			manner: 'plosive',
+			place: 'velar',
+			voiced: true,
+			freq: 1712/3183,
+		},
+	},
+	{
+		name: 'h',
+		properties: {
+			manner: 'approximant',
+			place: 'glottal',
+			voiced: false,
+			freq: 1703/3183,
+		},
+	},
+	{
+		name: 'i',
+		properties: {
+			isVowel: true,
+			freq: 2779/3183,
+		},
+	},
+	{
+		name: 'j',
+		properties: {
+			manner: 'approximant',
+			place: 'palatal',
+			voiced: true,
+			freq: 2716/3183,
+		},
+	},
 	{
 		name: 'k',
 		properties: {
@@ -10,12 +95,56 @@ const phones = [
 		},
 	},
 	{
+		name: 'l',
+		properties: {
+			manner: 'approximant',
+			place: 'alveolar',
+			voiced: true,
+			lateral: true,
+			freq: 2044/3183,
+		},
+	},
+	{
+		name: 'm',
+		properties: {
+			manner: 'nasal',
+			place: 'bilabial',
+			voiced: true,
+			freq: 2914/3183,
+		},
+	},
+	{
+		name: 'n',
+		properties: {
+			manner: 'nasal',
+			place: 'alveolar',
+			voiced: true,
+			freq: 2349/3183,
+		},
+	},
+	{
+		name: 'o',
+		properties: {
+			isVowel: true,
+			freq: 1826/3183,
+		},
+	},
+	{
 		name: 'p',
 		properties: {
 			manner: 'plosive',
 			place: 'bilabial',
 			voiced: false,
 			freq: 2594/3183,
+		},
+	},
+	{
+		name: 'q',
+		properties: {
+			manner: 'plosive',
+			place: 'uvular',
+			voiced: false,
+			freq: 256/3183,
 		},
 	},
 	{
