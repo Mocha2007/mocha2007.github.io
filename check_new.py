a274ac7d3ed461dcd976393567a6deb63af6dc1f
Mocha <3129210+Mocha2007@users.noreply.github.com>
2022-02-05 10:29:39 -0500

    check_new tool for me

---
+++ check_new.py
@@ -0,0 +1,15 @@
+# a small script to check which files are new or modified in the past 30 days
+from glob import glob
+from os.path import getctime, getmtime
+from time import time
+
+paths = glob('*.html', recursive=True)
+within30d = time() - 30 * 24 * 60 * 60
+for path in paths:
+	c = getctime(path)
+	if within30d < c:
+		print(f"C {c} - {path}")
+		continue
+	m = getmtime(path)
+	if within30d < m:
+		print(f"M {m} - {path}")
