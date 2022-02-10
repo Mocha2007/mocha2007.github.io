c8d2ac346ff3d067c221e1268557e2b5919a9495
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-02-09 22:36:29 -0500

    fixed recursion

--- check_new.py
+++ check_new.py
@@ -1,11 +1,13 @@
 # a small script to check which files are new or modified in the past 30 days
 from glob import glob
-from os.path import getctime, getmtime
+from os.path import abspath, dirname, getctime, getmtime
 from time import time
 
 day = 24 * 60 * 60
 limit = 30 # days
-for path in glob('*.html', recursive=True):
+my_path = dirname(abspath(__file__))
+for path in glob(my_path + '/**/*.html', recursive=True):
+	path = path.replace(my_path + '\\', '')
 	c = round((time() - getctime(path))//day)
 	if c < limit:
 		print(f"C {str.rjust(str(c), 2)} d ago - {path}")
