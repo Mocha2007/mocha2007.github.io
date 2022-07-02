2546f1a048be34d7423cc745e18c6574e67261a4
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-07-01 22:11:40 -0400

    soytest

--- NEL/mocha.js
+++ NEL/mocha.js
@@ -6,55 +6,59 @@ desc:'https://mocha2007.github.io/NEL/mocha.js',
 engineVersion:1,
 manifest:'modManifest.js',
 requires:['Default dataset*'],
-sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+sheets:{'mochaSheet':'mochaSheet.png',},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 {
 	//The idea in this simple example mod is to add a few elements focused around hot sauce, because hot sauce is great and I use that stuff everywhere.
 	
 	//First we create a couple new resources :
 	new G.Res({
-		name:'hot pepper',
+		name:'soybean',
 		desc:'[hot pepper]s are loaded with capsaicin and, depending on who you ask, may produce a pleasant burn when eaten.',
-		icon:[0,0,'spicySheet'],
+		icon:[0,0,'mochaSheet'],
 		turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
 		partOf:'food',
 		category:'food',
 	});
 	new G.Res({
-		name:'hot sauce',
-		desc:'Made from [herb]s and the [hot pepper,Spiciest peppers], this [hot sauce] stays fresh for a while and will leave anyone panting and asking for more.',
-		icon:[1,0,'spicySheet'],
-		turnToByContext:{'eat':{'health':0.03,'happiness':0.1},'decay':{'hot sauce':0.95,'spoiled food':0.05}},//that last part makes hot sauce effectively have a 95% chance of simply not rotting (in effect, it decays into itself)
+		name:'soy sauce',
+		desc:'Made from [soybean]s, this [soy sauce] stays fresh for a while and will leave everyone asking for more.',
+		icon:[1,0,'mochaSheet'],
+		turnToByContext:{'eat':{'health':0.03,'happiness':0.1},'decay':{'soy sauce':0.95,'spoiled food':0.05}},//that last part makes hot sauce effectively have a 95% chance of simply not rotting (in effect, it decays into itself)
 		partOf:'food',
 		category:'food',
 	});
 	
 	//Then we augment the base data to incorporate our new resources :
 		//adding hot pepper as something that can be gathered from grass
-	G.getDict('grass').res['gather']['hot pepper']=3;
+	G.getDict('grass').res['gather']['soybean']=1;
 		//adding a new mode to artisans so they can make hot sauce from hot peppers
-	G.getDict('artisan').modes['hot sauce']={name:'Make hot sauce',desc:'Turn 3 [hot pepper]s and 3 [herb]s into 1 [hot sauce].',req:{'hot sauce preparing':true},use:{'knapped tools':1}};
+	G.getDict('artisan').modes['soy sauce']={name:'Make soy sauce',desc:'Turn 1 [soybean], 1 [water], and 1 [salt] into 1 [soy sauce].',req:{'soy sauce preparing':true},use:{'knapped tools':1}};
 		//adding a new effect to artisans that handles the actual hot sauce preparing and is only active when the unit has the mode "hot sauce"
 	G.getDict('artisan').effects.push({type:'convert',from:{'hot pepper':3,'herb':3},into:{'hot sauce':1},every:3,mode:'hot sauce'});
+	G.getDict('artisan').effects.push({type:'convert',from:{'soybean':1,'water':1,'salt':1},into:{'soy sauce':1},every:1,mode:'soy sauce'});
 	
 	//Then we add a new technology which is required by the artisans to gain access to the "hot sauce" mode :
 	new G.Tech({
-		name:'hot sauce preparing',
+		name:'soy sauce preparing',
 		desc:'@[artisan]s can now produce [hot sauce] from [hot pepper]s and [herb]s//This special recipe allows a skilled craftsman to fully express the complex aromas present in hot peppers.',
-		icon:[0,1,'spicySheet'],
+		icon:[0,1,'mochaSheet'],
 		cost:{'insight':10},
 		req:{'cooking':true},
 	});
 	
 	//Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Hot sauce preparing" tech) is fulfilled.
 	new G.Trait({
-		name:'hot sauce madness',
-		desc:'@your people appreciate [hot sauce] twice as much and will be twice as happy from consuming it.',
-		icon:[1,1,'spicySheet'],
+		name:'soyboyism',
+		desc:'@your people appreciate [soy] products twice as much and will be twice as happy from consuming it.',
+		icon:[1,1,'mochaSheet'],
 		chance:20,
-		req:{'hot sauce preparing':true},
+		req:{'soy sauce preparing':true},
 		effects:[
-			{type:'function',func:function(){G.getDict('hot sauce').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
+			{type:'function',func:function(){
+				G.getDict('soy').turnToByContext['eat']['happiness']=0.2;
+				G.getDict('soy sauce').turnToByContext['eat']['happiness']=0.2;
+			}},//this is a custom function executed when we gain the trait
 		],
 	});
 	
