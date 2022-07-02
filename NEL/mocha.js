e12503c4ea412cc6c895291f60f68eac7a628520
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-07-02 04:24:04 -0400

    Update mocha.js

--- NEL/mocha.js
+++ NEL/mocha.js
@@ -170,8 +170,9 @@ func:function()
 			console.debug(res, agent);
 			const unit = G.unitsOwned.filter(u => u.unit.name === agent
 				// [                  gotta make sure this exists              ]
-				&& u.unit.effects[u.mode.num] && u.unit.effects[u.mode.num].into
-				&& u.unit.effects[u.mode.num].into[res])[0];
+				&& u.unit.effects[u.mode.num] && (u.unit.effects[u.mode.num].into
+												? u.unit.effects[u.mode.num].into[res] : true
+			)[0]);
 			if (G.resByName[res].amount < minRes && G.getUnitAmount(agent) < maxAgent)
 				unit.targetAmount = maxAgent;
 			else if (maxRes < G.resByName[res].amount && minAgent < G.getUnitAmount(agent))
