ea3376e3351d0be4bb7faceed827d6b3e484b254
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-07-11 19:12:22 -0400

    another quote

--- namei/erequote.js
+++ namei/erequote.js
@@ -4,13 +4,15 @@
 const eremorans = {
 	philosopher1: 'Urisbazêsur Badmar, philosopher',
 	philosopher2: 'Temêsur Saiur, princess and philosopher',
+	poet1: 'Lusimarisur Siurisar, poet',
 };
 
 const quotes = [
-	['Afêkkrum i hanum din id namboz:', 'Philosophy devours the unready.', eremorans.philosopher2],
-	['Dir nau sudôtamaz ne:', 'What\'s to be done?', eremorans.philosopher1],
-	['Mor su uid hod nau su i kekisuk tukilk ne:', 'Of what is the soul of gods if not man?', eremorans.philosopher2],
-	['Som ardo din ardo som din:', 'Heat from fire; fire from heat.', '<abbr title="Metalsmith\'s Guild">Aramaêsur Temêmor</abbr> motto'],
+	['Afêkkrum i hanum din id namboz', 'Philosophy devours the unready.', eremorans.philosopher2],
+	['Dir nau sudôtamaz ne', 'What\'s to be done?', eremorans.philosopher1],
+	['Libam su zurkudoid bêm i dir nau sudôz tan', 'The worst part about crying is determining what to do after.', eremorans.poet1],
+	['Mor su uid hod nau su i kekisuk tukilk ne', 'Of what is the soul of gods if not man?', eremorans.philosopher2],
+	['Som ardo din ardo som din', 'Heat from fire; fire from heat.', '<abbr title="Metalsmith\'s Guild">Aramaêsur Temêmor</abbr> motto'],
 	// todo common Enklár Shibboleth
 ];
 
