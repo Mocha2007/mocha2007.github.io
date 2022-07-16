0a431d9d33d715eb6b03347b19e511461884fa33
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-07-16 14:24:42 -0400

    fixed eremoran_export

--- namei/eremoran_export.js
+++ namei/eremoran_export.js
@@ -7,14 +7,14 @@ function exportDict(){
 	// converts the <dl> to a txt file and saves it for download
 	// const elements.d is the dl
 	let entry = {};
-	let entryString = '';
 	let afterDefList = false;
+	const dictionary = []; // list of objects
 	Array.from(elements.d.children).forEach(elem => {
 		// console.debug(elem.tagName);
 		switch (elem.tagName){
 			case 'DT':
 				// push last def
-				entryString += entryToString(entry);
+				dictionary.push(entry);
 				// reset
 				entry = {
 					title: elem.innerHTML,
@@ -23,33 +23,38 @@ function exportDict(){
 				break;
 			case 'DD':
 				if (elem.children.length
-					&& Array.from(elem.children)[0].tagName === 'ol'){
+					&& Array.from(elem.children)[0].tagName === 'OL'){
 					// must be a deflist
-					entry.defList = defListToString(elem);
+					entry.defList = defListToObj(elem.children[0]);
 					afterDefList = true;
 				}
+				else if (elem.children.length
+					&& Array.from(elem.children)[0].tagName === 'IMG')
+					// must be an image
+					entry.img = elem.children[0].src;
 				// must be word cat or notes or etym...
 				else if (afterDefList) // notes
 					entry.notes = elem.innerHTML;
 				// must be word cat or etym...
 				else if (elem.innerHTML.toLowerCase().includes('etymology')) // etym
-					entry.etym = elem.innerHTML;
+					entry.etym = elem.innerHTML.replace('Etymology: ', ''); // redundant
 				else
 					entry.cat = elem.innerHTML;
 				break;
 		}
 	});
 	// now we got our string in entryString
-	download(entryString, 'eremoran.txt', 'text/plain');
+	download(JSON.stringify(dictionary), 'eremoran.json', 'application/json');
 }
 
 /**
  * @param {HTMLOListElement} elem
  */
-function defListToString(elem){
-	return Array.from(elem.children).map(li => li.innerHTML).join(';');
+function defListToObj(elem){
+	return Array.from(elem.children).map(li => li.innerHTML); // .join(';')
 }
 
+/*
 function entryToString(entry){
 	let s = `${entry.title}=${entry.cat}=${entry.defList}`;
 	if (s.etym)
@@ -58,6 +63,7 @@ function entryToString(entry){
 		s += `;!${s.notes}`;
 	return s;
 }
+*/
 
 /* MAXIMAL EXAMPLE
 esokhu=v, 1=approach, attempt, try;!e + sokhu
