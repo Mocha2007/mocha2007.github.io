c8bdda28f299ffbf5427be42eea2319bd545d7ae
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-09-02 22:15:43 -0400

    more

--- timeline/timelineData.js
+++ timeline/timelineData.js
@@ -25,11 +25,39 @@ const timelineData = [
 		href: 'https://en.wikipedia.org/wiki/Leon_Trotsky',
 	},
 	{
+		name: 'Lev Kamenev',
+		start: '18 July 1883',
+		end: '25 August 1936',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Lev_Kamenev',
+	},
+	{
+		name: 'Grigory Zinoviev',
+		start: '23 September 1883',
+		end: '25 August 1936',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Grigory_Zinoviev',
+	},
+	{
+		name: 'Vyacheslav Molotov',
+		start: '9 March 1890',
+		end: '8 November 1986',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Vyacheslav_Molotov',
+	},
+	{
 		name: 'Nikita Khrushchev',
 		start: '15 April 1894',
 		end: '11 September 1971',
 		color: 'red',
 		href: 'https://en.wikipedia.org/wiki/Nikita_Khrushchev',
+	},
+	{
+		name: 'Georgy Zhukov',
+		start: '1 December 1896',
+		end: '18 June 1974',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Georgy_Zhukov',
 	},
 	{
 		name: 'Leonid Brezhnev',
@@ -38,10 +66,31 @@ const timelineData = [
 		color: 'red',
 		href: 'https://en.wikipedia.org/wiki/Leonid_Brezhnev',
 	},
+	{
+		name: 'Konstantin Chernenko',
+		start: '24 September 1911',
+		end: '10 March 1985',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Konstantin_Chernenko',
+	},
+	{
+		name: 'Yuri Andropov',
+		start: '15 June 1914',
+		end: '9 February 1984',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Yuri_Andropov',
+	},
+	{
+		name: 'Mikhail Gorbachev',
+		start: '2 March 1931',
+		end: '30 August 2022',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Mikhail_Gorbachev',
+	},
 ];
 
 // markers
-range(1870, 1991, 10).forEach(i => {
+range(1870, 2031, 10).forEach(i => {
 	const p = {
 		name: i,
 		start: `1 January ${i}`,
