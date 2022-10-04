aee91e424bf337794a3c09532a87ad9aece412b7
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-10-03 20:20:44 -0400

    identity.js

---
+++ identity.js
@@ -0,0 +1,57 @@
+const data = [
+	{
+		p: 'Death calls.',
+		choices: ['Accept incongruity', 'Fight back'],
+	},
+	{
+		p: 'You hear the voices of your colleagues around you, whispering behind your backs&mdash;they are disgusted by your appearance.',
+		choices: ['They&apos;re right&mdash;I&apos;m a freak.', 'But... they&apos;re wrong! I can&apos; help who I am!'],
+	},
+	{
+		p: 'You see your body morphing in front of your very eyes, into a new, grotesque form.',
+		choices: ['Accept transformation', 'Search for your true self'],
+	},
+	{
+		p: 'You look into the mirror, but do not recognize your reflection.',
+	},
+	{
+		p: 'You feel mold growing on your face.',
+	},
+	{
+		p: 'Your feel your clothes burning your skin.',
+	},
+	{
+		p: 'Your existence disgusts your family.',
+		choices: ['Accept their will', 'Struggle to retain your individuality'],
+	},
+];
+
+function identity(){
+	if (0.1 < Math.random())
+		return;
+	// import CSS
+	const style = document.createElement('link');
+	style.href = 'css/identity.css';
+	style.rel = 'stylesheet';
+	document.head.appendChild(style);
+	// block rest of doc
+	document.body.classList.add('noOverflow');
+	const div = document.createElement('div');
+	div.id = 'identity';
+	const datum = data[Math.floor(Math.random() * data.length)];
+	const p = document.createElement('p');
+	p.innerHTML = datum.p;
+	div.appendChild(p);
+	const choices = datum.choices || data[0].choices;
+	choices.forEach(choice => {
+		const span = document.createElement('span');
+		span.onclick = () => {
+			div.remove();
+			document.body.classList.remove('noOverflow');
+		};
+		span.innerHTML = choice;
+		div.appendChild(span);
+	});
+	document.body.appendChild(div);
+}
+identity();
