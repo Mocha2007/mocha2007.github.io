14ef4fc3feaf1f4b3ded1f8c933ff2a7672e409b
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-11-16 15:17:17 -0500

    remove edge spaces from gloss

--- namei/eremoran.js
+++ namei/eremoran.js
@@ -664,7 +664,7 @@ function linkCard(s){
 function gloss(s){
 	try {
 		const o = elements.raws.find(entry => entry.title === s).defList[0]
-			.replace(/\(.+\)/g, '') // remove parentheticals from glosses
+			.replace(/ ?\(.+\) ?/g, '') // remove parentheticals from glosses
 			.replace(/,.+/, ''); // remove secondary glosses
 		if (20 < o.length)
 			console.warn(`long gloss: ${s} = "${o}"`);
