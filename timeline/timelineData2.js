7e1b8b5e29ba94d154e476cdcc6677e17240f7d1
mocha <3129210+Mocha2007@users.noreply.github.com>
2022-12-07 11:03:22 -0500

    more test data

---
+++ timeline/timelineData2.js
@@ -0,0 +1,89 @@
+/* exported timelineTest */
+
+const timelineTest = [
+	// people
+	{
+		name: 'Vladimir Lenin',
+		start: '22 April 1870',
+		end: '21 January 1924',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Vladimir_Lenin',
+	},
+	{
+		name: 'Joseph Stalin',
+		start: '18 December 1878',
+		end: '5 March 1953',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Joseph_Stalin',
+	},
+	{
+		name: 'Leon Trotsky',
+		start: '7 November 1879',
+		end: '21 August 1940',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Leon_Trotsky',
+	},
+	{
+		name: 'Lev Kamenev',
+		start: '18 July 1883',
+		end: '25 August 1936',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Lev_Kamenev',
+	},
+	{
+		name: 'Grigory Zinoviev',
+		start: '23 September 1883',
+		end: '25 August 1936',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Grigory_Zinoviev',
+	},
+	{
+		name: 'Vyacheslav Molotov',
+		start: '9 March 1890',
+		end: '8 November 1986',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Vyacheslav_Molotov',
+	},
+	{
+		name: 'Nikita Khrushchev',
+		start: '15 April 1894',
+		end: '11 September 1971',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Nikita_Khrushchev',
+	},
+	{
+		name: 'Georgy Zhukov',
+		start: '1 December 1896',
+		end: '18 June 1974',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Georgy_Zhukov',
+	},
+	{
+		name: 'Leonid Brezhnev',
+		start: '19 December 1906',
+		end: '10 November 1982',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Leonid_Brezhnev',
+	},
+	{
+		name: 'Konstantin Chernenko',
+		start: '24 September 1911',
+		end: '10 March 1985',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Konstantin_Chernenko',
+	},
+	{
+		name: 'Yuri Andropov',
+		start: '15 June 1914',
+		end: '9 February 1984',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Yuri_Andropov',
+	},
+	{
+		name: 'Mikhail Gorbachev',
+		start: '2 March 1931',
+		end: '30 August 2022',
+		color: 'red',
+		href: 'https://en.wikipedia.org/wiki/Mikhail_Gorbachev',
+	},
+];
