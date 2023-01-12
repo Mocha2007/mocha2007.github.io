91762becd0b14c4354de42670087ebe66c65fbfe
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-01-12 08:36:12 -0500

    better colors

--- mine/mineData.js
+++ mine/mineData.js
@@ -1,9 +1,9 @@
 /* global Block, Item */
 new Block('bedrock', '#444');
 new Block('grass', 'green');
-new Block('dirt', 'brown');
+new Block('dirt', '#840');
 new Block('stone', 'grey');
 new Block('wood', 'brown');
 new Block('coal', '#222');
-new Block('iron_ore', '#f88');
+new Block('iron_ore', '#fa8');
 new Item('torch');
