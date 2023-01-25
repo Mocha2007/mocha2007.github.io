300cef386a63c4bbdaf5510426886170c1acabe1
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-01-24 22:09:24 -0500

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -1,7 +1,29 @@
 /* exported blog */
 
 class Blogpost {
-	// todo
+	/**
+	 * @param {string} title
+	 * @param {Date} date
+	 * @param {Tag[]} tags
+	 * @param {string} innerHTML
+	 */
+	constructor(title, date, tags, innerHTML){
+		this.title = title;
+		this.date = date;
+		this.tags = tags;
+		this.innerHTML = innerHTML;
+	}
+	/** @param {string} s */
+	static parse(s){
+		// todo
+	}
+}
+
+class Tag {
+	/** @param {string} name */
+	constructor(name){
+		this.name = name;
+	}
 }
 
 const blog = {
