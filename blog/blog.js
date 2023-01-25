8ee8b9f5b972aa40bebf1aac2d1d3fc97bd28b77
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-01-24 22:15:29 -0500

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -5,13 +5,13 @@ class Blogpost {
 	 * @param {string} title
 	 * @param {Date} date
 	 * @param {Tag[]} tags
-	 * @param {string} innerHTML
+	 * @param {Section[]} sections
 	 */
-	constructor(title, date, tags, innerHTML){
+	constructor(title, date, tags, sections){
 		this.title = title;
 		this.date = date;
 		this.tags = tags;
-		this.innerHTML = innerHTML;
+		this.sections = sections;
 	}
 	/** @param {string} s */
 	static parse(s){
@@ -23,6 +23,35 @@ class Tag {
 	/** @param {string} name */
 	constructor(name){
 		this.name = name;
+	}
+	get elem(){
+		const e = document.createElement('span');
+		e.classList.add('tag');
+		// e.onclick = () =>
+		e.innerHTML = this.name;
+		return e;
+	}
+}
+
+class Section {
+	/**
+	 * @param {Tag[]} tags
+	 * @param {string} innerHTML
+	 */
+	constructor(tags, innerHTML){
+		this.tags = tags;
+		this.innerHTML = innerHTML;
+	}
+	get elem(){
+		const div = document.createElement('div');
+		const span = document.createElement('span');
+		span.innerHTML = 'Tags: ';
+		this.tags.forEach(t => span.appendChild(t.innerHTML));
+		div.appendChild(span);
+		const p = document.createElement('p');
+		p.innerHTML = this.innerHTML;
+		div.appendChild(p);
+		return div;
 	}
 }
 
