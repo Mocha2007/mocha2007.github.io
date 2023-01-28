37dbd0ccfbc4c839d77a51832090f37de8c301d5
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-01-28 09:10:52 -0500

    Update sudoku.js

--- tools/sudoku.js
+++ tools/sudoku.js
@@ -229,7 +229,7 @@ class Sudoku {
 				for (let i = 0; i < squareSize; i++)
 					for (let j = 0; j < squareSize; j++)
 						board.data[squareSize*ii+i][squareSize*jj+j]
-							= order[squareSize*((i+jj*ai+bi)%squareSize)+((j+ii*aj+bj)%squareSize)];
+							= order[squareSize*((i+jj*ai+bi)%squareSize) + (j+ii*aj+bj)%squareSize];
 		// phase 2: attempt to introduce randomness by deleting every other tile
 		for (let i = 0; i < size; i++)
 			for (let j = 0; j < size; j++)
