8f065d1124c616b0ec485a246f1cfe8156ec1d90
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-03-22 12:35:37 -0400

    Update check_new.py

--- check_new.py
+++ check_new.py
@@ -3,15 +3,16 @@ from glob import glob
 from os.path import abspath, dirname, getctime, getmtime
 from time import time
 
-day = 24 * 60 * 60
-limit = 30 # days
-my_path = dirname(abspath(__file__))
-for path in glob(my_path + '/**/*.html', recursive=True):
-	path = path.replace(my_path + '\\', '')
-	c = round((time() - getctime(path))//day)
-	if c < limit:
+DAY = 24 * 60 * 60
+LIMIT = 30 # days
+PATH = dirname(abspath(__file__))
+
+for path in glob(PATH + '/**/*.html', recursive=True):
+	path = path.replace(PATH + '\\', '')
+	c = round((time() - getctime(path))//DAY)
+	if c < LIMIT:
 		print(f"C {str.rjust(str(c), 2)} d ago - {path}")
 		continue
-	m = round((time() - getmtime(path))//day)
-	if m < limit:
+	m = round((time() - getmtime(path))//DAY)
+	if m < LIMIT:
 		print(f"M {str.rjust(str(m), 2)} d ago - {path}")
