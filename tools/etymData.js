9a527858778bc78b0c68166e99910fd96d199de9
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-05-04 23:03:16 -0400

    Update etymData.js

--- tools/etymData.js
+++ tools/etymData.js
@@ -3,15 +3,41 @@
 // shortcuts
 const EDS = {
 	greek: 'Ancient Greek',
+	latin: 'Latin',
+	mocha: '(mochanoises)',
 };
 
 const etymData = [
 	// make these alphabetical
 	{
+		head: '-árkhēs',
+		lang: EDS.greek,
+		gloss: 'leader',
+		forms: ['arch'],
+	},
+	{
+		head: '-íā',
+		lang: EDS.greek,
+		gloss: '(noun-forming suffix)',
+		forms: ['ia'],
+	},
+	{
 		head: '-ṓtēs',
 		lang: EDS.greek,
 		gloss: '(noun-forming suffix)',
-		forms: ['ota'],
+		forms: ['ota', 'ta'],
+	},
+	{
+		head: 'an-',
+		lang: EDS.greek,
+		gloss: 'un-',
+		forms: ['an', 'a'],
+	},
+	{
+		head: 'amníon',
+		lang: EDS.greek,
+		gloss: 'bowl',
+		forms: ['amnio'],
 	},
 	{
 		head: 'arkhaîos',
@@ -20,10 +46,124 @@ const etymData = [
 		forms: ['archaeo', 'archae'],
 	},
 	{
+		head: 'Boréās',
+		lang: EDS.greek,
+		gloss: 'north',
+		forms: ['boreo'],
+	},
+	{
+		head: 'deúteros',
+		lang: EDS.greek,
+		gloss: 'second',
+		forms: ['deutero'],
+	},
+	{
+		head: 'dís',
+		lang: EDS.greek,
+		gloss: 'twice',
+		forms: ['di'],
+	},
+	{
+		head: 'eǘs',
+		lang: EDS.greek,
+		gloss: 'good',
+		forms: ['eu'],
+	},
+	{
+		head: 'gnáthos',
+		lang: EDS.greek,
+		gloss: 'jaw',
+		forms: ['gnatho', 'gnath'],
+	},
+	{
+		head: 'haplóos',
+		lang: EDS.greek,
+		gloss: 'single',
+		forms: ['haplo'],
+	},
+	{
+		head: 'hólos',
+		lang: EDS.greek,
+		gloss: 'whole',
+		forms: ['holo'],
+	},
+	{
 		head: 'ikhthū́s',
 		lang: EDS.greek,
 		gloss: 'fish',
 		forms: ['ichthy'],
+	},
+	{
+		head: 'káruon',
+		lang: EDS.greek,
+		gloss: 'nut',
+		forms: ['kary'],
+	},
+	{
+		head: 'khoánē',
+		lang: EDS.greek,
+		gloss: 'funnel',
+		forms: ['choano'],
+	},
+	{
+		head: 'khordḗ',
+		lang: EDS.greek,
+		gloss: 'guts',
+		forms: ['chord'],
+	},
+	{
+		head: 'katá',
+		lang: EDS.greek,
+		gloss: 'down',
+		forms: ['cata'],
+	},
+	{
+		head: 'kontós',
+		lang: EDS.greek,
+		gloss: 'pole',
+		forms: ['kont'],
+	},
+	{
+		head: 'méros',
+		lang: EDS.greek,
+		gloss: 'part',
+		forms: ['mere', 'mero'], // telomere, meronym
+	},
+	{
+		head: 'metá',
+		lang: EDS.greek,
+		gloss: 'amid',
+		forms: ['meta'],
+	},
+	{
+		head: 'morphḗ',
+		lang: EDS.greek,
+		gloss: 'shape',
+		forms: ['morph'],
+	},
+	{
+		head: 'néos',
+		lang: EDS.greek,
+		gloss: 'new',
+		forms: ['neo'],
+	},
+	{
+		head: 'nephrós',
+		lang: EDS.greek,
+		gloss: 'kidney',
+		forms: ['nephro'],
+	},
+	{
+		head: 'opísthios',
+		lang: EDS.greek,
+		gloss: 'rear',
+		forms: ['opistho'],
+	},
+	{
+		head: 'orthós',
+		lang: EDS.greek,
+		gloss: 'straight',
+		forms: ['ortho'],
 	},
 	{
 		head: 'ostéon',
@@ -32,9 +172,107 @@ const etymData = [
 		forms: ['osteo', 'oste', 'ost'], // osteoarthritis, osteichthyes, teleost
 	},
 	{
+		head: 'pará',
+		lang: EDS.greek,
+		gloss: 'beside',
+		forms: ['para'],
+	},
+	{
+		head: 'pnoḗ',
+		lang: EDS.greek,
+		gloss: 'breath',
+		forms: ['pno'],
+	},
+	{
+		head: 'poús',
+		lang: EDS.greek,
+		gloss: 'foot',
+		forms: ['podo', 'pod'],
+	},
+	{
 		head: 'Prōteús',
 		lang: EDS.greek,
 		gloss: 'Proteus',
 		forms: ['proteo'],
 	},
+	{
+		head: 'prôtos',
+		lang: EDS.greek,
+		gloss: 'first',
+		forms: ['proto'],
+	},
+	{
+		head: 'pterón',
+		lang: EDS.greek,
+		gloss: 'feather',
+		forms: ['ptero'],
+	},
+	{
+		head: 'ptérux',
+		lang: EDS.greek,
+		gloss: 'wing',
+		forms: ['pteryg'],
+	},
+	{
+		head: 'rhipídion',
+		lang: EDS.greek,
+		gloss: 'bellows',
+		forms: ['rhipid'],
+	},
+	{
+		head: 'rhī́s',
+		lang: EDS.greek,
+		gloss: 'nose',
+		forms: ['rhino'],
+	},
+	{
+		head: 'sárx',
+		lang: EDS.greek,
+		gloss: 'flesh',
+		forms: ['sarco'],
+	},
+	{
+		head: 'skótos',
+		lang: EDS.greek,
+		gloss: 'darkness',
+		forms: ['scoto'],
+	},
+	{
+		head: 'stóma',
+		lang: EDS.greek,
+		gloss: 'mouth',
+		forms: ['stom'],
+	},
+	{
+		head: 'téleios',
+		lang: EDS.greek,
+		gloss: 'complete',
+		forms: ['tele'], // teleost
+	},
+	{
+		head: 'tetra-',
+		lang: EDS.greek,
+		gloss: 'four',
+		forms: ['tetra'],
+	},
+	{
+		head: 'thēríon',
+		lang: EDS.greek,
+		gloss: 'beast',
+		forms: ['theria'],
+	},
+	{
+		head: 'zôion',
+		lang: EDS.greek,
+		gloss: 'animal',
+		forms: ['zoo', 'zoa', 'zo'],
+	},
+	/*
+	{
+		head: '(mochanoise)',
+		lang: EDS.mocha,
+		gloss: 'gay',
+		forms: ['kippi', 'boi', 'snueg'],
+	},
+	*/
 ];
