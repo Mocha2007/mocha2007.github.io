84665be1db15f47fa403a309661ae86502c5460b
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-05-10 11:02:41 -0400

    Update ipaData.js

--- tools/ipaData.js
+++ tools/ipaData.js
@@ -1,12 +1,49 @@
 /* exported ipaData */
 
+const constants = {
+	polish_vowels: 'aeiouyąęó',
+};
+
 const ipaData = [
 	{
 		lang: 'Polish',
 		rules: [
-			// todo
 			// consonants
-			['ć', 't͡ɕ'],
+			[`ć|ci(?=[${constants.polish_vowels}])`, 't͡ɕ'],
+			['c?h', 'x'],
+			['cz', 't͡ʂ'],
+			['dż', 'd͡ʐ'],
+			[`dź|dzi(?=[${constants.polish_vowels}])`, 'd͡ʑ'],
+			['n(?=[gk])', 'ŋ'],
+			[`ń|ni(?=[${constants.polish_vowels}])`, 'ɲ'],
+			[`ś|si(?=[${constants.polish_vowels}])`, 'ɕ'],
+			['sz', 'ʂ'],
+			['w', 'v'],
+			['ż|rz', 'ʐ'],
+			[`ź|zi(?=[${constants.polish_vowels}])`, 'ʑ'],
+			// consonants, round 2
+			['c', 't͡s'],
+			['dz', 'd͡z'],
+			['ł', 'w'],
+			// oral vowels
+			['e', 'ɛ'],
+			['o', 'ɔ'],
+			['ó', 'u'],
+			['y', 'ɨ'],
+			// nasal vowels
+			// https://en.wikipedia.org/wiki/Polish_phonology#Distribution_2
+			['ą(?=[bp])', 'ɔm'],
+			['ą(?=[td])', 'ɔn'],
+			['ą(?=[kg])', 'ɔŋ'],
+			['ą(?=[lw])', 'ɔ'],
+			['ę(?=[bp])', 'ɛm'],
+			['ę(?=[td])', 'ɛn'],
+			['ę(?=[kg])', 'ɛŋ'],
+			['ę(?=[lw])', 'ɛ'],
+			['ę$', 'ɛ'],
+			// else...
+			['ą', 'ɔ̃'],
+			['ę', 'ɛ̃'],
 		],
 	},
 ];
