426c2aa69804ae97992300aa1fc4db43a1df4834
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-11-28 12:07:22 -0500

    feature size

--- namei/nee.js
+++ namei/nee.js
@@ -133,12 +133,18 @@ class Feature {
 }
 /** @type {Feature[]} */
 Feature.features = [];
+const featureSize = {
+	maj: 2 * nee.orbsize,
+	semi: 1.6 * nee.orbsize,
+	mount: 1.2 * nee.orbsize,
+	min: nee.orbsize,
+}
 // countries / nations / cities
 // murans
 new Feature(1517/3000, 1872/3000, 'Eremor', 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Kirthar_park.jpg',
-	50, 'Eremor is an interesting land...');
+	featureSize.maj, 'Eremor is an interesting land...');
 new Feature(1290/3000, 1660/3000, 'Mur', 'https://upload.wikimedia.org/wikipedia/commons/6/62/TigrisRiver.JPG',
-	25, 'The Murans are related to the Eremorans.');
+	featureSize.min, 'The Murans are related to the Eremorans.');
 // near murans
 new Feature(1630/3000, 1890/3000, 'Tepu', 'https://upload.wikimedia.org/wikipedia/commons/6/66/Periyar_National_Park.JPG');
 new Feature(1740/3000, 1860/3000, 'Kuren', 'https://upload.wikimedia.org/wikipedia/commons/2/23/Sundarban_Tiger.jpg');
@@ -147,19 +153,19 @@ new Feature(1670/3000, 1700/3000, 'Wu', 'https://upload.wikimedia.org/wikipedia/
 new Feature(1760/3000, 1600/3000, 'Dako', 'https://upload.wikimedia.org/wikipedia/commons/6/66/Kumrat_valley%2C_Upper_Dir.jpg');
 new Feature(1100/3000, 1340/3000, 'Irikar', 'https://upload.wikimedia.org/wikipedia/commons/2/23/Azerbaijan_Nature_Shaki.jpg');
 // other majors
-new Feature(2640/3000, 1560/3000, 'Važcud', 'https://upload.wikimedia.org/wikipedia/commons/2/2a/%E4%B8%B9%E9%9C%9E%E5%B1%B1_04.jpg', 50, '', ['dead']);
-new Feature(1680/3000, 1060/3000, 'Iwemälre', 'https://savannahinkyrgyzstan.files.wordpress.com/2013/08/dsc_0234.jpg', 50, '', ['dead']);
-new Feature(2070/3000, 1630/3000, 'Nekang', 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Tak_province_Burmese_mountains.jpg', 50);
-new Feature(1650/3000, 1390/3000, 'Nuzdexax', 'https://upload.wikimedia.org/wikipedia/commons/3/3b/FrontLines_Environment_Photo_Contest_Winner_-5_%285808476109%29.jpg', 50);
-new Feature(740/3000, 890/3000, 'Półtaś', 'https://upload.wikimedia.org/wikipedia/commons/2/29/Green_and_rolling_countryside_in_northern_Portugal_%2852712607091%29.jpg', 50);
-new Feature(910/3000, 550/3000, 'Porön', 'https://upload.wikimedia.org/wikipedia/commons/9/91/German_Countryside_%2873864589%29.jpeg', 50);
-new Feature(2230/3000, 2800/3000, 'Nizhind', 'https://upload.wikimedia.org/wikipedia/commons/4/46/Lake_Toba%2C_North_Sumatra%2C_Indonesia.jpg', 50);
-new Feature(1200/3000, 1080/3000, '"PlainsLang"', 'https://upload.wikimedia.org/wikipedia/commons/7/74/Patagonian_Steppe_%283260842962%29.jpg', 50);
-new Feature(230/3000, 1900/3000, '"Boizone"', 'https://i.imgflip.com/7tz40i.jpg', 50, 'The Boizone (name to be determined) is where much of Pankair\'s supply of millet comes from.');
+new Feature(2640/3000, 1560/3000, 'Važcud', 'https://upload.wikimedia.org/wikipedia/commons/2/2a/%E4%B8%B9%E9%9C%9E%E5%B1%B1_04.jpg', featureSize.maj, '', ['dead']);
+new Feature(1680/3000, 1060/3000, 'Iwemälre', 'https://savannahinkyrgyzstan.files.wordpress.com/2013/08/dsc_0234.jpg', featureSize.maj, '', ['dead']);
+new Feature(2070/3000, 1630/3000, 'Nekang', 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Tak_province_Burmese_mountains.jpg', featureSize.semi);
+new Feature(1650/3000, 1390/3000, 'Nuzdexax', 'https://upload.wikimedia.org/wikipedia/commons/3/3b/FrontLines_Environment_Photo_Contest_Winner_-5_%285808476109%29.jpg', featureSize.semi);
+new Feature(740/3000, 890/3000, 'Półtaś', 'https://upload.wikimedia.org/wikipedia/commons/2/29/Green_and_rolling_countryside_in_northern_Portugal_%2852712607091%29.jpg', featureSize.maj);
+new Feature(910/3000, 550/3000, 'Porön', 'https://upload.wikimedia.org/wikipedia/commons/9/91/German_Countryside_%2873864589%29.jpeg', featureSize.maj);
+new Feature(2230/3000, 2800/3000, 'Nizhind', 'https://upload.wikimedia.org/wikipedia/commons/4/46/Lake_Toba%2C_North_Sumatra%2C_Indonesia.jpg', featureSize.maj);
+new Feature(1200/3000, 1080/3000, '"PlainsLang"', 'https://upload.wikimedia.org/wikipedia/commons/7/74/Patagonian_Steppe_%283260842962%29.jpg', featureSize.maj);
+new Feature(230/3000, 1900/3000, '"Boizone"', 'https://i.imgflip.com/7tz40i.jpg', featureSize.maj, 'The Boizone (name to be determined) is where much of Pankair\'s supply of millet comes from.');
 // topological features
-new Feature(840/3000, 1070/3000, 'Sinônô', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Karachay-Cherkessia%2C_Caucasus_Mountains%2C_Abishira-Akhuba%2C_Black_Rock_Ridge_2.jpg', 40,
+new Feature(840/3000, 1070/3000, 'Sinônô', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Karachay-Cherkessia%2C_Caucasus_Mountains%2C_Abishira-Akhuba%2C_Black_Rock_Ridge_2.jpg', featureSize.mount,
 	'The great western mountain range.');
-new Feature(1900/3000, 1310/3000, 'Nadnekis', 'https://upload.wikimedia.org/wikipedia/commons/2/29/Himalayas%2C_Ama_Dablam%2C_Nepal.jpg', 40,
+new Feature(1900/3000, 1310/3000, 'Nadnekis', 'https://upload.wikimedia.org/wikipedia/commons/2/29/Himalayas%2C_Ama_Dablam%2C_Nepal.jpg', featureSize.mount,
 	'The great eastern mountain range.');
 
 nee.main();
