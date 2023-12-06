552b5400f6dee8626e9f1e22088783427fb39cfb
mocha <3129210+Mocha2007@users.noreply.github.com>
2023-12-06 11:31:05 -0500

    Update cal.js

--- tools/cal.js
+++ tools/cal.js
@@ -152,7 +152,8 @@ const time = {
 			CELTIC: ['1 FEB', '1 MAY', '1 AUG', '1 NOV'],
 			CH: ['2 MAR', '10 JUN', '9 SEP', '1 DEC'],
 			DRYWET: ['1 JAN', '1 MAY', '1 MAY', '1 NOV', '1 NOV'], // idk how this works, but it does!!!
-			METEO: ['1 MAR', '1 JUN', '1 SEP', '1 DEC'],
+			METEON: ['1 MAR', '1 JUN', '1 SEP', '1 DEC'],
+			METEOS: ['1 JAN', '1 JAN', '1 MAR', '1 JUN', '1 SEP', '1 DEC', '1 MAR', '1 JUN'], // ALMOST works... just fails for december
 			MID: ['3 FEB', '6 MAY', '8 AUG', '6 NOV'], // Set midpoint to sol/equ, like Pliny the Elder
 			PA: ['8 MAR', '9 JUN', '6 SEP', '7 DEC'], // LEHIGHTON
 			ROMAN: ['7 FEB', '9 MAY', '11 AUG', '10 NOV'], // Varro
