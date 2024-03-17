c49001b4ea2836ef8bb1070b4618c4bdbe5c3b90
mocha <3129210+Mocha2007@users.noreply.github.com>
2024-03-17 15:03:20 -0400

    pl test

---
+++ tools/pl.js
@@ -0,0 +1,126 @@
+/* exported PL, PL_LOADED */
+
+class Declension {
+	constructor(o){
+		// s
+		this.snom = o.s.nom;
+		this.sgen = o.s.gen;
+		this.sdat = o.s.dat;
+		this.sacc = o.s.acc;
+		this.sins = o.s.ins;
+		this.sloc = o.s.loc;
+		this.svoc = o.s.voc;
+		// pl
+		this.plnom = o.pl.nom;
+		this.plgen = o.pl.gen;
+		this.pldat = o.pl.dat;
+		this.placc = o.pl.acc;
+		this.plins = o.pl.ins;
+		this.plloc = o.pl.loc;
+		this.plvoc = o.pl.voc;
+	}
+	get elem(){
+		// todo
+	}
+}
+
+const PL = {
+	decl: {
+		f(index){
+			// https://en.wiktionary.org/wiki/Appendix:Polish_nouns#Feminine_nouns
+			const decl_o = {s: {nom: index}, pl: {}};
+			const stem = index.slice(0, index.length-1);
+			// Genitive singular adds -y.
+			decl_o.s.gen = stem + 'y';
+			// Dative singular palatalizes (softens) the final consonant cluster
+			// and adds -e, -i or -y.
+			// Locative singular is always the same as dative singular.
+			decl_o.s.loc = decl_o.s.dat = this.fdat(index);
+			// Accusative singular adds -ę.
+			decl_o.s.acc = stem + 'ę';
+			// Instrumental singular adds -ą.
+			decl_o.s.ins = stem + 'ą';
+			// Vocative singular adds -o.
+			decl_o.s.voc = stem + 'o';
+			// todo nom pl annoyance
+			decl_o.pl.voc = decl_o.pl.acc = decl_o.pl.nom = this.fpl(index);
+			decl_o.pl.gen = stem;
+			decl_o.pl.dat = stem + 'om';
+			decl_o.pl.ins = stem + 'ami';
+			decl_o.pl.loc = stem + 'ach';
+			return new Declension(decl_o);
+		},
+		/** @param {string} word */
+		fdat(word){
+			const rule = this.fdat_dat.find(r => new RegExp(r[0]).test(word));
+			return word.replace(new RegExp(rule[0]), rule[1]);
+		},
+		// https://en.wiktionary.org/wiki/Appendix:Polish_nouns#List_of_palatalizations_in_the_dative_and_locative
+		fdat_dat: [
+			// 4c
+			['dzia$', 'dzi'],
+			// 3c
+			['bia$', 'bi'],
+			['cha$', 'sze'],
+			['cia$', 'ci'],
+			['cza$', 'czy'],
+			['dza$', 'dzy'],
+			['dża$', 'dży'],
+			['pia$', 'pi'],
+			['rza$', 'rzy'],
+			['sia$', 'si'],
+			['sła$', 'śle'],
+			['sma$', 'śmie'],
+			['sna$', 'śnie'],
+			['sta$', 'ście'],
+			['sza$', 'szy'],
+			['zda$', 'ździe'],
+			['zia$', 'zi'],
+			['zła$', 'źle'],
+			['zma$', 'zmie'],
+			['zna$', 'źnie'],
+			// 2c
+			['ba$', 'bie'],
+			['ca$', 'cy'],
+			['da$', 'dzie'],
+			['fa$', 'fie'],
+			['ga$', 'dze'],
+			['ja$', 'ji'],
+			['ka$', 'ce'],
+			['la$', 'li'],
+			['ła$', 'le'],
+			['ma$', 'mie'],
+			['na$', 'nie'],
+			['pa$', 'pie'],
+			['ra$', 'rze'],
+			['sa$', 'sie'],
+			['ta$', 'cie'],
+			['wa$', 'wie'],
+			['za$', 'zie'],
+			['ża$', 'ży'],
+		],
+		/** @param {string} word */
+		fpl(word){
+			const rule = this.fpl_dat.find(r => new RegExp(r[0]).test(word));
+			return word.replace(new RegExp(rule[0]), rule[1]);
+		},
+		fpl_dat: [
+			['(?<=[kg])a$', 'i'],
+			['(?<=[mbpwfndtzsłrh])a$', 'y'],
+			['a$', 'e'],
+		],
+	},
+	get_final_cons_cluster(word){
+		// todo
+		return word;
+	},
+	main(){
+		// todo
+	},
+	pal_final_cons_cluster(word){
+		// todo
+		return word;
+	},
+};
+
+const PL_LOADED = true;
