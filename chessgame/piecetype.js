d1c047d621d491d7a680762654e9765d79b15acd
mocha <3129210+Mocha2007@users.noreply.github.com>
2024-05-10 11:00:42 -0400

    Update piecetype.js

--- chessgame/piecetype.js
+++ chessgame/piecetype.js
@@ -1,11 +1,29 @@
 /* exported PieceType */
 
 class PieceType {
+	/**
+	 * @param {string} name
+	 * @param {string} abbr
+	 * @param {string} white_emoji
+	 * @param {string} black_emoji
+	 * @param {MovementType} movement
+	 */
 	constructor(name, abbr, white_emoji, black_emoji, movement){
 		this.name = name || 'unknown';
 		this.abbr = abbr || '?';
 		this.white_emoji = white_emoji || '?';
 		this.black_emoji = black_emoji || '?';
 		this.movement = movement;
+	}
+}
+
+class MovementType {
+	constructor(both, move = [], capture = []){
+		this.both = both;
+		this.move = move;
+		this.capture = capture;
+	}
+	validMoves(coords, board){
+		// todo
 	}
 }
