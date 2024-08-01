f06bc6e439b855ae3fd2fabe3cc77a55c48be1ad
mocha <3129210+Mocha2007@users.noreply.github.com>
2024-08-01 11:15:32 +0200

    enderal table

--- tools/enderal_minigame.js
+++ tools/enderal_minigame.js
@@ -1,5 +1,5 @@
-/* exported run */
-/* global random, range, sum */
+/* exported recTable, run */
+/* global random, range, round, sum */
 const BOOST_RATIO = 0.425; // "between 40 and 45%"
 const AI_ROLLS_DICE_THRESHOLD_LOWER = -11; // https://web.archive.org/web/20211220170105/https://www.reddit.com/r/enderal/comments/rkrrsx/battle_for_treomar_is_really_unfair_an_analysis/
 const AI_ROLLS_DICE_THRESHOLD_UPPER = -1;
@@ -26,7 +26,7 @@ function test(player_hand, player_bet, opponent_bet, n = 10000){
 			value += boost();
 			boosted = true;
 		}
-		const result = value > PLAYER ? 'LOSS' : value < PLAYER ? 'WIN' : 'DRAW';
+		const result = value > PLAYER ? 'LOSS' : 'WIN';
 		return {value, boosted, result};
 	}
 	// simulate opponent hands
@@ -34,7 +34,7 @@ function test(player_hand, player_bet, opponent_bet, n = 10000){
 	const they_boosted = sum(opponent_hands.map(x => x.boosted));
 	const WIN = 100/n*opponent_hands.filter(x => x.result === 'WIN').length;
 	const LOSS = 100/n*opponent_hands.filter(x => x.result === 'LOSS').length;
-	const DRAW = 100/n*opponent_hands.filter(x => x.result === 'DRAW').length;
+	// const DRAW = 100/n*opponent_hands.filter(x => x.result === 'DRAW').length;
 	// console.info('WIN % = ', 100 * WIN / n);
 	// console.info('LOSS % = ', 100 * LOSS / n);
 	// console.info('DRAW % = ', 100 * DRAW / n);
@@ -47,7 +47,7 @@ function test(player_hand, player_bet, opponent_bet, n = 10000){
 	const they_boostedB = sum(opponent_handsB.map(x => x.boosted));
 	const WINB = 100/n*opponent_handsB.filter(x => x.result === 'WIN').length;
 	const LOSSB = 100/n*opponent_handsB.filter(x => x.result === 'LOSS').length;
-	const DRAWB = 100/n*opponent_handsB.filter(x => x.result === 'DRAW').length;
+	// const DRAWB = 100/n*opponent_handsB.filter(x => x.result === 'DRAW').length;
 	// console.info('YOU BOOSTED WIN % = ', 100 * WINB / n);
 	// console.info('YOU BOOSTED LOSS % = ', 100 * LOSSB / n);
 	// console.info('YOU BOOSTED DRAW % = ', 100 * DRAWB / n);
@@ -55,7 +55,7 @@ function test(player_hand, player_bet, opponent_bet, n = 10000){
 		+ they_boostedB/n*Math.round(BOOST_RATIO * opponent_bet);
 	// console.info('YOU BOOSTED EV = ', EVB);
 	const RECOMMENDATION = EVB > EV ? 'BOOST' : 'DO NOT BOOST';
-	return {WIN, LOSS, DRAW, EV, WINB, LOSSB, DRAWB, EVB, RECOMMENDATION};
+	return {WIN, LOSS, EV, WINB, LOSSB, EVB, RECOMMENDATION};
 }
 
 function run(){
@@ -68,11 +68,64 @@ function run(){
 	const RESULT = test(get('player_hand'), get('player_bet'), get('opponent_bet'));
 	set('WIN', RESULT.WIN);
 	set('LOSS', RESULT.LOSS);
-	set('DRAW', RESULT.DRAW);
 	set('EV', RESULT.EV);
 	set('WINB', RESULT.WINB);
 	set('LOSSB', RESULT.LOSSB);
-	set('DRAWB', RESULT.DRAWB);
 	set('EVB', RESULT.EVB);
 	set('RECOMMENDATION', RESULT.RECOMMENDATION);
+	document.getElementById('RECOMMENDATION').className = `REC_${RESULT.RECOMMENDATION[0]}`;
 }
+
+function speedTest(desired_time = 1000, needed_calculations = 280, n = 100){
+	const DEBUG_T = new Date();
+	for (let i = 0; i < needed_calculations; i++)
+		test(random.randint(3, 30), 5, 10, n);
+	const T = new Date() - DEBUG_T;
+	const o = Math.floor(n * desired_time / T);
+	// eslint-disable-next-line max-len
+	console.debug(`Tested ${needed_calculations} random calculations @ ${T} ms when n = ${n}; recommend n = ${o}`);
+	return o;
+}
+speedTest.reThink = () => {
+	const desired_time = 1000 * document.getElementById('thinkTime').value;
+	rec_n = speedTest(desired_time);
+	recTable();
+};
+let rec_n = 0;
+
+function recTable(){
+	const n = rec_n || speedTest();
+	const opponent_bet = 100;
+	const player_hands = range(3, 31); // 28 cols + header
+	const player_bets = range(5, 51, 5);
+	const TABLE = document.getElementById('recTable');
+	TABLE.innerHTML = '';
+	// headers
+	const TRH = document.createElement('tr');
+	TRH.innerHTML = '<th></th>';
+	TABLE.appendChild(TRH);
+	player_hands.forEach(player_hand => {
+		const TH = document.createElement('th');
+		TH.innerHTML = player_hand;
+		TRH.appendChild(TH);
+	});
+	// main rows
+	player_bets.forEach(player_bet => {
+		// header
+		const TR = document.createElement('tr');
+		TABLE.appendChild(TR);
+		const TH = document.createElement('th');
+		TH.innerHTML = round(player_bet/opponent_bet, 2);
+		TR.appendChild(TH);
+		// main
+		player_hands.forEach(player_hand => {
+			const TD = document.createElement('td');
+			const RESULT = test(player_hand, player_bet, opponent_bet, n);
+			TD.className = `REC_${RESULT.RECOMMENDATION[0]}`;
+			TD.innerHTML = RESULT.RECOMMENDATION === 'BOOST' ? '✔️' : '❌';
+			TR.appendChild(TD);
+		});
+	});
+}
+
+console.info('loaded enderal_minigame.js');
