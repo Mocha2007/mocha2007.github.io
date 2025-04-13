9ce562657d07d55c5b2ca46776e2eee46629340d
mocha <3129210+Mocha2007@users.noreply.github.com>
2025-04-13 12:14:09 +0200

    voynich wordle

---
+++ tools/voynich_wordle.js
@@ -0,0 +1,85 @@
+/* exported wordle */
+/* global random */
+
+const corpus = 'shory ckhar cthar sheky cthes oteey oteos daiin chear cphar cphoy oydar yshey shody oschy cfhol teody ydain cphey ytain oksho kshoy oteol ckhey cthey okain kchom tchey pshol dydyd dchar potol cthal shear kaiin cthol chtor choty kchsy cfhar lkody chody potoy choky chodo cheol kechy dchor otcho chkar chtod saiin daind dchol fodan sheey qokey ykody dalor ckhor cthor chety chyky ykeey pchor otchy cholo cheor kchor chees qopal daimm ochor schey qokol cthom sheol octhy okeey ekshy soeom okeom qopol qoeor cphor qotoy ckhol seees qodar tchor cfham ykchy okeol daiim okary ytchy kcham sheam yteam qotey doiin shain sheyr soiin ksheo kchoy dolds olaen qooko shoky qotol qokoy fchoy chkoy oaiin dkshy ochey ckhoy otain pshod chols chots daiir qotor shees okchy taiin cphol chckh kshor ykeor sheor yaiir ytaly ckham ychol pchar chory odshe ychar kaiim cheas kchey qoair okody ytody ychos ychor qokom kchdy qokam qokor ckhal ytcho kchos dchey chald ychey oaiir dshoy kchod cheey oeear deees sheod chopy tchty okche choto choor pchey daird kshey chsey qokar chean schol cthod cheal dolar dshol chary cthan kchol orchl keoky satar cheen chcky raiin tydlo choly toiin qotod dayty tchol olsho qofol oeees toldy ypchy ctchy rokyd chyty chair doiir osain paiin otydy cheky tshol qodor chodl soydy opchy ytshy dpchy qokhy ykoly daldy koair qokod okshy qocky qotyd dairo shoin okald olchy ockhy soshy fchol koiin shoty oeeen ykshy dchyd dardy tshor okear poror orshy okeor otody sheal ykair toror cphod sholo chydy sokar daram opydy ydair qodam opydg ypaim schar keeor oekor poiis kchar oytor chkor qoain diiin chlol oshor doldy olody dchal qokay ykcho tolol daror cthyd darom ychoy qochy daral ochol dchod faiis cheos sheoy keody chosy otyky yteol ykeea chtol lchol olshy okoly ocphy ofchy schor oldor cphal oldam dachy dshor fshor teeol ychky yfchy daing ldals ykain ykyka otchm oldal olsar otshy oldar kshol olols oloro odain qotar eteor qekar chtar qodol qoear okeer chcth okeal sshey ydals okcho tchar qodom qokch oeeey ochdy chrky shair chain otosy orcho dshey chkey chedy ytedy haiin shedy cthhy otedy tcheo geedy okedy shese sheos etedy ckhdy saldy eeeos deeol sches qokes coain chals keeod dchir shkol chyqo pchol daiis cheod ychom olals chshy tchos ykchn choko deeey scheo rchey dcheo chkol dairy oyshy keeol sheeo yches ykedy saiir oteor keedy ykeeo okees lkedy ykeos cheef ychek lchey okeos seoar ekeey cheom ytsho schea shdor loiin chdal qakar yteey komdy tokar tchdy ycheo tshdy orain porar darar okaly yfody oltey chesy ysair aikhy aiiin qokal loees dasam orair dykey seear chdar yshos cthoo dainl lchal koldy chiin ykeol gaiin fchom otaro ytydy qoshy otoly oscho yokor sholy qotal tolor ychok keeey chshd eeedy dchdy okair laiin dshdy chekl chdam pdair oshdy pardy oraly oekar fchor sarol lchdy lokar yaiin kcheo cfhdy karar todar qoeed chkal pchdy aithy toees faimy ofair arody ypchd ychdy schdy shepy qokyl qokeg ykeod toshy yshol qopor cheam olcha kshes ckhhy qocho shols sodal cpheo sarar odeey folor cphhy dasar kshdy kedar otary tshed sheet arodl poldy teedy alody chtey aldar chepy olkey choal oteod kaldy taros koees otota qotsh roiry ololy ytoda keees lchor dairl lchar olald shoto pchom cthdy fchdy karal qokdy techy lshed ytasr aicky tchod ctham daiiy dcthy keesy otald qoteo lotal dalmy alkey ypair oteed lkeey otees loldy chdor dodar qotan cheet chodr oriin odchy koddy okeod dchsy dchog ariin dario dalol ykald oshol yoees qodal ykhor ytair qotam tchom dchom kodam shoar oqoor sodar dcham cphom qotea otsho ckhos choek chpal yteal aldam alchy okchd olkam oiiin dodyd oeeed talam pchal ykaky otaky esedy kchal poeeo cheop sheep yodam dchos odair tcfhy soeos dkals dydas echty tedas otaly shaly oteom arary ealam chaly dalar todal csees alkar kshar yteor dalal dytal salol dalam alols ykaly faiir fchey salal olaly otals poaly talar olain oishy polal choar yfair qoais keoar dalas otair otaim shety chpdy sheek cheot ykcol daoly qolsa raral shddy cheeo chcta cheer kardy pchof cheyl chxar lkeol kodal qokyr dakar ydees epchy sainn ychsy oshey sotey otoky ykchs sochy otocs saral dorar dakan yteos soshe ofydy yfain soeor ydoly oeeos oirsg sakar cfhey solal oeola airar ykalo olkao ekais aiiny siiin aroka oekey otchl chpor okady oriid okemy shdar shchy oeeeo odchd okyld doaro otory otydm qooto seeey kodar olsey okeeg eeody qoeey oteem okosy darol salar shsdy yteod teeos opain araly okeeo ithor ylcho okeea aloly otamy yteeo chsdy chese ykady cheoy oaram yekey chsar doair otoar ytyda ytary oeesa ytory tshey okosd ykeal okseo ykees rkeeo ockhh oches chess forar chtam oteeo ykemy shshy qetal yokal shkal alain okydy okasy otard aildy otyly asaly oteas qorky osheo parar ateey teeey shsky otchr chtos shldy ainam yroly otais orary chchs oheol okoey oekes cheem alkam ofais oiral orara shses yfary ytoar octho soees otchs teoda chsor daein ogeom chcpy dchyr cheis oeemy oleeo soear oeeod yteed sandy oltar oeedy opalg olkar otear odody chpar oiiny ypain okery chefy csedy odees opals alshy oiair ylaly ltain lkamo lshey qokan ithey shtol pshar qoked ltedy sorol ychty saino dainy qopdy rshey taral oloin qolol dokal qoted olked oqoky olkol tedar lkain qotes qeeey okeed qolal talor okeyr qokas kolpy loral rorol chdol qoyky cpchy roiin deedy dytey fcham rshed shtal shets qeedy otork soral sheed dchee oloky okalo shdal qolky tedal ekedy keoly qotas rched sokol alory yched daiil tchal olkee dalky ytees opaly orory spchy lshdy eesey loeey dkedy oltal sheds solor oikhy okaos orkar olkal chkam pshdy ykail qolar cther dkeey skain okolo okory solky chtal dalom cseey totol oroly lteey lolom oklor talkl otlol oithy qorar qoker okiin qcthy okais dkain dched ykeda ltary rshdy qoldy otoin lched sshol lolol ikhey dolol tokol otdal lokam rches qsain lkeed soldy sairn qotyl qekey rtain okail lsheg qoeeo lldar olohy doiis opoly ofoly aryly skeey ithhy poiin cseol korol cheel lolor yskhy dolor shdol yoiin okshd tarar qopar tdain polky shtor olfor olfar olkor qotom soiis ochar qeeor scody opcho oepor oteer qoeol olair oleey taiky keear ldaly qckhy qofod damom lshar qoedy oltam lfchy shdam cheeg otshs chsky lkesy ytdar ytaim qoody ytapy cthos todor atolg yksho orald sheom otyda qokeo daimd raldy yshor ldain chead odory cheyd oeeor coeky cheys cheok cheea okeoy kosar oteee eeeey lcheo doral qekor okols qokos chool cfhoy chofy ckheo ctheo keeos okeam qkhey ycham teols teeys ckcho schos cthog oteoc opary ykaro kaedy tshod odaly podar arcsy qoksh kshed sheyk tshes lshee ytear sosar sshor yckhy ooror opsho qkory oeksa okesy osary doror osaro soity sosam sofal oreso odeor rolsy eesos dalsy ckhod qkeey ssheo chchy cholp dokor otala chedo teeal qkhol deeor doeey lsais airam sossy kolor desey sachy soiir aiees oreey ykory ypory daisn rchol saiim deody chsea ofcho lkeor kasol oteys lkldy lotos seedy lcheg okool lotar chalr lkees lshor olldy oteal qkain otech okaim otalr oeeal shtey opair airod chllo aisam ysheo lkeeo qoete shoda alchd qotir qoeky oteoy dlchd dadam qchor lsair chaim lkair cfheo arals arair olcho akain roiir lkchs cheed qodos qodan oesal sairy teeor paiir opail airal raiis aiidy ofory chkas fodal ookam roees olord chdos lklor lpaim sheoe arxor oeoar dshes pcheo qoiin chlar rcheo aiiry yshar poral akair dlchy aloal loain amchy lsheo shkar loror sheas shoor tolos saram dychy kalos ysoir chady qeeoy aiphy taror pchod otiir kairy ralky opaim rkeey ychkl todky alkal aifhy taidy dckhy dlkal palar sokod dyair aiily pokar keeeo kaiis soain opasy kedal qaiin qetam chlam tedam lkeal shkhy alkan alold chedl porol cseam arain loety chlal raram deeal leeey qeeal reeey shtar loedy lkaim tched shkey cheda qeear chalg ltchs polar talol tarol raldl lchsl qekal otail solol amchd oteeg alair ochos polor pched famom eotar lkchy shchs alchl sokal rchsy qopam lchod akchy lodar qchar okhar taram dsheo qoees oriim loary teoar roral fchee arald ofchr lteam airol teear keeed eedol dkchs orold kolky octhd dkair oeain chopo oaiil airoy oeail shxam qotos oedal shoko alkol oeeoy oraro chsho llsan lksho rchar lchea qotad otchd oleed rodam xoiin ralom lshes lraly sheot chlor chddy lcham tshar losdy llchs rchos llory padar rteol lrain cheds chery cphan aleey sykar'.split(' ');
+
+const wordle = {
+	/** @type {string} */
+	current: undefined,
+	guesses: 0,
+	initialized: false,
+	/** @param {string} char */
+	append(char){
+		document.getElementById('wordle_input').value += char;
+	},
+	/** @param {string} word */
+	fixWord(word){
+		return word.toLowerCase();
+	},
+	/** @param {string} guess */
+	guessElement(guess){
+		const span = document.createElement('span');
+		guess.split('').forEach((char, i) => {
+			const elem = document.createElement('span');
+			elem.innerHTML = char;
+			elem.classList.add(`wordle${this.rateLetter(char, i)}`);
+			span.appendChild(elem);
+		});
+		return span;
+	},
+	init(){
+		if (!this.initialized){
+			document.getElementById('wordle_input').addEventListener('keydown', e => {
+				if (e.key === 'Enter')
+					this.submit();
+			});
+			this.initialized = true;
+			const button_container = document.getElementById('buttons');
+			'abcdefghijklmnopqrstuvwxyz'.split('').forEach(c => {
+				const span = document.createElement('span');
+				button_container.appendChild(span);
+				span.innerHTML = c;
+				span.className = 'voynich button';
+				span.onclick = () => wordle.append(c);
+			});
+		}
+		this.reset();
+	},
+	/** @returns {string} */
+	randomWord(){
+		return random.choice(corpus);
+	},
+	rateLetter(letter, position){
+		return this.current[position] === letter ? 2
+			: this.current.includes(letter) ? 1 : 0;
+	},
+	reset(){
+		document.getElementById('wordle_input').value = '';
+		document.getElementById('wordle_history').innerHTML = '';
+		this.current = this.randomWord();
+		this.guesses = 0;
+	},
+	submit(){
+		/** @type {string} */
+		const guess = this.fixWord(document.getElementById('wordle_input').value);
+		// verify 5 valid chars
+		if (!this.validate(guess))
+			return window.alert('Invalid Word!');
+		if (5 < this.guesses)
+			return window.alert(`Too many guesses! The word was ${this.current}.
+			Press Reset to try again!`);
+		// add to history
+		const li = document.createElement('li');
+		li.appendChild(this.guessElement(guess));
+		document.getElementById('wordle_history').appendChild(li);
+		// clear text box
+		document.getElementById('wordle_input').value = '';
+		this.guesses++;
+		// refocus
+		document.getElementById('Wordle').scrollIntoView();
+	},
+	/** @param {string} guess */
+	validate(guess){
+		return /[a-z]{5}/.test(guess);
+	},
+};
