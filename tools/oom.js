75f8e2d03fe721bd7b402141c79563577e181f7f
Mocha <mocha2007@users.noreply.github.com>
2026-02-06 19:45:05 +0100

    more OwO

--- tools/oom.js
+++ tools/oom.js
@@ -112,6 +112,7 @@ const CONSTANT = {
 	/** in kg */
 	da: 1.66053906892e-27,
 	density: {
+		ice: 916.75,
 		iron: 7874,
 		rock: 2500,
 		/** kg/m^3, at 25 C */
@@ -120,35 +121,56 @@ const CONSTANT = {
 	},
 	/** in m/s https://physics.nist.gov/cgi-bin/cuu/Value?c */
 	c: 299792458,
+	/** in s */
+	get d(){
+		return this.h*60;
+	},
+	/** in kg */
 	get dr(){
 		return this.lb/256;
 	},
 	/** in J */
 	eV: 1.602176634e-19,
+	/** in kg */
 	get gr(){
 		return this.lb/7000;
+	},
+	/** in s */
+	get h(){
+		return this.min*60;
 	},
 	/** in kg */
 	jupiter_mass: 1.898125e27,
 	/** in kg */
 	lb: 0.45359237,
+	/** in kg */
 	get long_ton(){
 		return 2240*this.lb;
 	},
+	/** in kg */
 	get MeVc2(){
 		return 1e6*this.eV/Math.pow(this.c, 2);
 	},
+	/** in s */
+	min: 60,
+	/** in kg */
 	get oz(){
 		return this.lb/16;
 	},
 	/** in J*s */
 	planck: 6.62607015e-34,
+	/** in kg */
 	get short_ton(){
 		return 2000*this.lb;
 	},
 	si_prefix: "qryzafpnμm kMGTPEZYRQ",
 	si_prefix_offset: 10,
+	/** in kg */
 	solar_mass: 1988475000e21,
+	/** in s */
+	get yr(){
+		return this.d*365.2425;
+	},
 };
 
 const OOM = {
@@ -165,6 +187,7 @@ const OOM = {
 		new MassDatum("Ultraviolet mass equivalence", 6e-6*CONSTANT.MeVc2),
 		new MassDatum("X-ray (soft) mass equivalence", 100e-6*CONSTANT.MeVc2),
 		new MassDatum("Gamma ray (minimum) mass equivalence", 10e-3*CONSTANT.MeVc2),
+		new MassDatum("Joule mass equivalence", 1/Math.pow(CONSTANT.c, 2)),
 		// Subatomic particles
 		new MassDatum("Muon Neutrino (upper bound)", 0.17e-6*CONSTANT.MeVc2, "https://en.wikipedia.org/wiki/Neutrino#Flavor,_mass,_and_their_mixing"),
 		new MassDatum("Electron Neutrino (upper bound)", 0.80e-6*CONSTANT.MeVc2, "https://en.wikipedia.org/wiki/Neutrino#Flavor,_mass,_and_their_mixing"),
@@ -236,6 +259,9 @@ const OOM = {
 		new MassDatum("Mitochondrion", 1.0754507267897768e-17*CONSTANT.density.water), // "Mitochondria are commonly between 0.75 and 3 μm2 in cross section,"
 		new MassDatum("Chloroplast", 20e-18*CONSTANT.density.water, "https://en.wikipedia.org/wiki/Chloroplast#Structure"), // "Corn seedling chloroplasts are ≈20 μm3 in volume"
 		new MassDatum("Nucleus (Human)", 4/3*Math.PI*Math.pow(3e-6,3)*CONSTANT.density.water, "https://en.wikipedia.org/wiki/Cell_nucleus#Nuclear_structures_and_landmarks"), // "In human cells, the diameter of the nucleus is approximately six micrometres (μm)"
+		// Body parts
+		new MassDatum("Heart (Human)", 0.3, "https://en.wikipedia.org/wiki/Heart#Location_and_shape"),
+		new MassDatum("Liver (Human)", 1.5, "https://en.wikipedia.org/wiki/Liver#Structure"),
 		// vaguely human-sized
 		new MassDatum("Planck mass", 2.176434e-8, "https://en.wikipedia.org/wiki/Planck_units"),
 		new MassDatum("Dime (US)", 2.268e-6),
@@ -271,6 +297,7 @@ const OOM = {
 		new MassDatum("Cat", 4.5, "https://en.wikipedia.org/wiki/Cat#Size"),
 		new MassDatum("Human (Infant)", 3.5, "https://en.wikipedia.org/wiki/Birth_weight"),
 		new MassDatum("Human (Global average)", 62, "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3408371"),
+		new MassDatum("Human biomass", 62*8.165e9, "https://www.census.gov/popclock/world"),
 		new MassDatum("Elephant (African)", (7e3+3.6e3)/2, "https://hypertextbook.com/facts/2003/EugeneShnayder.shtml"),
 		new MassDatum("Whale (Blue)", 150e3, "https://hypertextbook.com/facts/2003/MichaelShmukler.shtml"),
 		// Vehicles
@@ -287,11 +314,19 @@ const OOM = {
 		new MassDatum("Destroyer (Arleigh Burke-class)", 9e3*CONSTANT.long_ton, "https://en.wikipedia.org/wiki/Arleigh_Burke-class_destroyer"),
 		new MassDatum("Titanic", 52.31e3*CONSTANT.long_ton, "https://en.wikipedia.org/wiki/Titanic"),
 		new MassDatum("Aircraft carrier (Gerald R. Ford-class)", 100e3*CONSTANT.long_ton, "https://en.wikipedia.org/wiki/Gerald_R._Ford-class_aircraft_carrier"),
+		new MassDatum("Seawise Giant", 646642*CONSTANT.long_ton, "https://en.wikipedia.org/wiki/Seawise_Giant"),
 		// misc big things
+		new MassDatum("Great Bell of Dhammazedi", 294e3, "https://en.wikipedia.org/wiki/Great_Bell_of_Dhammazedi"),
 		new MassDatum("Pando", 6e6, "https://en.wikipedia.org/wiki/Pando_(tree)"),
 		new MassDatum("Great Pyramid of Giza", 6e9, "https://en.wikipedia.org/wiki/Great_Pyramid_of_Giza"),
+		new MassDatum("Annual global coal production (2019)", 7921e3*1e3, "https://en.wikipedia.org/wiki/Coal_mining#Production"),
+		new MassDatum("Annual global aluminum production (2024)", 72000*1e6, "https://en.wikipedia.org/wiki/Aluminium#Production_and_refinement"),
+		new MassDatum("Annual global iron ore production (2015)", 2280000000*1e3, "https://en.wikipedia.org/wiki/Iron_ore#Production_and_consumption"),
+		new MassDatum("Danube annual discharge", 6452*CONSTANT.density.water*CONSTANT.yr, "https://en.wikipedia.org/wiki/Danube"),
 		new MassDatum("Earth's biosphere", 1841e15, "https://hypertextbook.com/facts/2001/AmandaMeyer.shtml"),
+		new MassDatum("Greenland ice sheet", 1710000e6*1673*CONSTANT.density.ice, "https://en.wikipedia.org/wiki/Greenland_ice_sheet"),
 		new MassDatum("Earth's atmosphere", 5e18, "https://hypertextbook.com/facts/1999/LouiseLiu.shtml"),
+		new MassDatum("Antarctic ice sheet", 14e6*1e6*2200*CONSTANT.density.ice, "https://en.wikipedia.org/wiki/Antarctic_ice_sheet"),
 		new MassDatum("Earth's oceans", 1.4e21, "https://hypertextbook.com/facts/1998/AvijeetDut.shtml"),
 		new MassDatum("Earth's core", 1e23, "https://en.wikipedia.org/wiki/Earth%27s_inner_core#Density_and_mass"),
 		// Astro
@@ -316,7 +351,8 @@ const OOM = {
 		new MassDatum("Charon", 1.5897e21),
 		// new MassDatum("Gonggong", 1.75e21),
 		new MassDatum("Iapetus", 1.8056591e21),
-		new MassDatum("Rhea", 2.3064854e21),
+		// new MassDatum("Rhea", 2.3064854e21),
+		new MassDatum("Asteroid Belt", 9.3839e20/.392, "https://en.wikipedia.org/wiki/Asteroid_belt#Characteristics"),
 		new MassDatum("Makemake", 2.69e21),
 		new MassDatum("Oberon", 3.1104e21),
 		new MassDatum("Titania", 3.4550e21),
@@ -339,7 +375,10 @@ const OOM = {
 		new MassDatum("Saturn", 5.68317e26),
 		new MassDatum("Jupiter", CONSTANT.jupiter_mass),
 		// Stars
+		new MassDatum("WISE 0855-0714", (3+10)/2*CONSTANT.jupiter_mass),
+		new MassDatum("Luhman 16 system", (35.4+29.4)*CONSTANT.jupiter_mass),
 		new MassDatum("Proxima Centauri", 0.1221*CONSTANT.solar_mass),
+		new MassDatum("Barnard's Star", 0.162*CONSTANT.solar_mass),
 		new MassDatum("Sun", CONSTANT.solar_mass),
 		new MassDatum("Chandrasekhar limit", 1.44*CONSTANT.solar_mass),
 		new MassDatum("Sirius A", 2.063*CONSTANT.solar_mass),
@@ -348,6 +387,7 @@ const OOM = {
 		new MassDatum("R136a1", 291*CONSTANT.solar_mass),
 		new MassDatum("Pleiades", 800*CONSTANT.solar_mass),
 		new MassDatum("M2", 1.04e5*CONSTANT.solar_mass),
+		new MassDatum("Sagittarius A*", 4.297e6*CONSTANT.solar_mass),
 		new MassDatum("Sagittarius Dwarf Galaxy", 4e8*CONSTANT.solar_mass),
 		new MassDatum("Small Magellanic Cloud", 7e9*CONSTANT.solar_mass),
 		new MassDatum("Large Magellanic Cloud", 1e10*CONSTANT.solar_mass),
