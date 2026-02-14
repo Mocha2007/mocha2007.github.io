e0f7f7de1d107f831e9fa5790b700d4fa5b26fdd
Mocha <mocha2007@users.noreply.github.com>
2026-02-14 18:09:45 +0100

    unit system deriver OwO

---
+++ tools/units.js
@@ -0,0 +1,147 @@
+class Dimension {
+	static TIME = "time";
+	static LENGTH = "length";
+	static MASS = "mass";
+	static CURRENT = "current";
+	static TEMPERATURE = "temperature";
+	static AMOUNT = "amount";
+	static LUMINOUS_INTENSITY = "luminous intensity";
+}
+
+class Derived {
+	constructor(sym, name, dimensions){
+		this.sym = sym;
+		this.name = name;
+		this.dimensions = dimensions;
+	}
+	/** @returns {HTMLSpanElement} */
+	get elem(){
+		return document.getElementById(`output_${this.name}`);
+	}
+}
+
+const UNITS = {
+	get all_units(){
+		// base units
+		const base = Object.values(Dimension).map(name => {
+			const sym = this.base_unit_symbols[name];
+			const dimensions = {};
+			dimensions[name] = 1;
+			return new Derived(sym, name, dimensions);
+		});
+		return base.concat(...this.derived);
+	},
+	base_unit_symbols: {
+		time: "s",
+		length: "m",
+		mass: "kg",
+		current: "A",
+		temperature: "K",
+		amount: "mol",
+		"luminous intensity": "cd",
+	},
+	derived: [
+		new Derived('N', 'force', {mass:1,length:1,time:-2}),
+	],
+	elem: {
+		create_dim_input(name, sym){
+			const label = document.createElement('label');
+			label.innerHTML = `${name}: `;
+			const input = document.createElement('input');
+			input.type = 'text';
+			input.placeholder = 1;
+			label.for = input.name = input.id = `input_${name}`;
+			label.appendChild(input);
+			label.innerHTML += sym;
+			label.onmouseup = label.onkeyup = () => UNITS.update();
+			return label;
+		},
+		create_dim_output(name, sym){
+			const container = document.createElement('div');
+			container.innerHTML = `${name}: `;
+			const printout = document.createElement('span');
+			printout.id = `output_${name}`;
+			container.appendChild(printout);
+			container.innerHTML += ` ${sym}`;
+			return container;
+		},
+		/** @type {HTMLDivElement} */
+		main: undefined,
+	},
+	init(){
+		const main = this.elem.main = document.getElementById('main');
+		const cont_pre = document.createElement('div');
+		main.appendChild(cont_pre);
+		const cont_in = document.createElement('div');
+		main.appendChild(cont_in);
+		const cont_out = document.createElement('div');
+		main.appendChild(cont_out);
+		// we need preset buttons
+		Object.keys(this.preset).forEach(system => {
+			const button = document.createElement('span');
+			button.classList.add('button');
+			button.innerHTML = system;
+			button.onclick = () => {
+				Object.keys(this.base_unit_symbols).forEach(bu => {
+					const elem = document.getElementById(`input_${bu}`);
+					elem.value = this.preset[system][bu] || '';
+				});
+				this.update();
+			};
+			cont_pre.appendChild(button);
+		});
+		// we need inputs for all 7 base units
+		Object.keys(this.base_unit_symbols).forEach(name => {
+			const sym = this.base_unit_symbols[name];
+			const elem = this.elem.create_dim_input(name, sym);
+			cont_in.appendChild(elem);
+		});
+		// ... and then output info for each derived unit!
+		this.derived.forEach(d => {
+			const elem = this.elem.create_dim_output(d.name, d.sym);
+			cont_out.appendChild(elem);
+		});
+		this.update(); // update once
+		console.info('units.js loaded.');
+	},
+	preset: {
+		metric: {
+			time: 1,
+			length: 1,
+			mass: 1,
+			current: 1,
+			temperature: 1,
+			amount: 1,
+			"luminous intensity": 1,
+		},
+		imperial: {
+			length: 0.3048,
+			mass: 0.45359237,
+			temperature: 5/9,
+		},
+		planck: {
+			time: 5.391247e-44,
+			length: 1.616255e-35,
+			mass: 2.176434e-8,
+			current: 5.290818e-19,
+			temperature: 1.416784e-32,
+		},
+	},
+	update(){
+		const base_unit_values = {};
+		Object.keys(this.base_unit_symbols).forEach(bu => {
+			const elem = document.getElementById(`input_${bu}`);
+			base_unit_values[bu] = parseFloat(elem.value || elem.placeholder);
+		});
+		// console.debug(`update -> `, base_unit_values);
+		this.derived.forEach(d => {
+			let product = 1.;
+			for (let key in d.dimensions){
+				product *= Math.pow(base_unit_values[key], d.dimensions[key]);
+			}
+			d.elem.innerHTML = product;
+		});
+	},
+};
+
+UNITS.init();
