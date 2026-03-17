70ca237c7e45acfd4a6123f7a3802fc8acd0a6d0
Mocha <mocha2007@users.noreply.github.com>
2026-03-17 13:50:18 +0100

    eg2

---
+++ emojiglyph2.js
@@ -0,0 +1,81 @@
+class Word {
+	constructor(gloss, root, marks_semantic, marks_syntactic, alternates, notes){
+		/** @type {string} */
+		this.gloss = gloss;
+		/** @type {string} */
+		this.root = root;
+		/** @type {string[]} */
+		this.marks_semantic = marks_semantic || [];
+		/** @type {string[]} */
+		this.marks_syntactic = marks_syntactic || [];
+		/** @type {Word[]} */
+		this.alternates = alternates || [];
+		/** @type {string} */
+		this.notes = notes || "";
+	}
+	elem(){
+		const e = document.createElement('span');
+		e.classList.add('word');
+		e.title = this.gloss;
+		// semantic marks
+		const mc = document.createElement('div');
+		mc.innerHTML = this.marks_semantic.map(EG2.charOrImg).join(' ');
+		e.appendChild(mc);
+		// root
+		const root = document.createElement('div');
+		root.innerHTML = EG2.charOrImg(this.root);
+		e.appendChild(root);
+		// syntactic marks
+		const sm = document.createElement('div');
+		sm.innerHTML = this.marks_syntactic.map(EG2.charOrImg).join(' ');
+		e.appendChild(sm);
+		return e;
+	}
+	tr(){
+		const o = document.createElement('tr');
+		const th = document.createElement('th');
+		th.innerHTML = this.gloss;
+		o.appendChild(th);
+		const col1 = document.createElement('td');
+		col1.appendChild(this.elem());
+		o.appendChild(col1);
+		const col2 = document.createElement('td');
+		this.alternates.forEach(alt => col2.appendChild(alt.elem()));
+		o.appendChild(col2);
+		const notes = document.createElement('td');
+		notes.innerHTML = this.notes;
+		o.appendChild(notes);
+		return o;
+	}
+}
+
+const EG2 = {
+	/** @param {string} s */
+	charOrImg(s){
+		return s.startsWith('http') ? `<img src="${s}">` : s;
+	},
+	init(){
+		const dict = document.getElementById('dictionary');
+		this.lexicon.forEach(w => dict.appendChild(w.tr()));
+		const sources = document.getElementById('sources');
+		this.sources.forEach(s => {
+			const li = document.createElement('li');
+			const a = document.createElement('a');
+			a.innerHTML = a.href = s;
+			li.appendChild(a);
+			sources.appendChild(li);
+		});
+		console.info('emojiglyph2.js loaded.');
+	},
+	lexicon: [
+		new Word('and', '&'),
+		new Word('at', '@'),
+		new Word('iron', '♂', ['🜃']),
+		new Word('Mars', '♂', ['🪐']),
+	],
+	sources: [
+		'https://en.wikipedia.org/wiki/Alchemical_symbol',
+	],
+};
+
+EG2.init();
