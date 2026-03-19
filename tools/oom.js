7b97e3bb20588e0b9cfb73006e2e1d560670d74e
Mocha <mocha2007@users.noreply.github.com>
2026-03-19 18:48:46 +0100

    gas

--- tools/oom.js
+++ tools/oom.js
@@ -1429,8 +1429,11 @@ const OOM = {
 		new MoneyDatum('Platinum (1 kg, 2026)', 2139.30/CONSTANT.ozt, "https://www.dailymetalprice.com/metaltables.php"),
 		new MoneyDatum('Gold (1 kg, 2026)', 5081.77/CONSTANT.ozt, "https://www.dailymetalprice.com/metaltables.php"),
 		// items
+		new MoneyDatum('Coca-Cola (US, 6.5 fl oz, 1886-1959, inflation-adjusted)', 0.5615, "https://en.wikipedia.org/wiki/Fixed_price_of_Coca-Cola_from_1886_to_1959"),
 		new MoneyDatum('Apple (Gala, US, 2026)', 0.68, "https://www.walmart.com/ip/Fresh-Gala-Apple-Each/44390953?classType=REGULAR&from=/search"),
 		new MoneyDatum('Costco Hot Dog (US, 1984-2026)', 1.5, "https://en.wikipedia.org/wiki/Costco_hot_dog"),
+		new MoneyDatum('Gas (US, 1 gal, 2026-03-19)', 3.884, "https://gasprices.aaa.com/todays-state-averages/"),
+		new MoneyDatum('Tank of Gas (US, 14 gal, 2026-03-19)', 14*3.884, "https://gasprices.aaa.com/todays-state-averages/"),
 		new MoneyDatum('Raspberry Pi 5 (Base model, US, 2026)', 45, "https://www.raspberrypi.com/products/"),
 		new MoneyDatum('iPhone 17 (US, 2026)', 799, "https://www.apple.com/iphone-17/"),
 		new MoneyDatum('Shahed drone (2026)', new Money({min: 20e3, max: 50e3}), "https://www.seattletimes.com/nation-world/irans-drones-cost-a-fraction-of-the-u-s-weapons-shooting-them-down/"),
