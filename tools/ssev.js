579b8017f1e7f3eeaa36b9a14bf29298c589a4fa
Mocha <mocha2007@users.noreply.github.com>
2026-04-01 18:22:26 +0200

    purple earth, also known as pearth

--- tools/ssev.js
+++ tools/ssev.js
@@ -325,9 +325,12 @@ const SSEV = {
 			new PlanetCoords(Time.fromEarthAge(-50), 1),
 			new PlanetCoords(new Time(0), 1),
 		), t => CONSTANTS.ageEarth-50 < t ? 'https://mocha2007.github.io/tools/ssev/proto.jpg'
-			: 3500 < t ? 'https://mocha2007.github.io/tools/ssev/earth_archaean.jpg'
 			// Glaciations
 			: CONSTANTS.glacial(t) ? 'https://mocha2007.github.io/tools/ssev/snowball.jpg'
+			// Archaean surface
+			: 3500 < t ? 'https://mocha2007.github.io/tools/ssev/earth_archaean.jpg'
+			// 2400-3500 Mya: https://en.wikipedia.org/wiki/Purple_Earth_hypothesis
+			: 2400 < t ? 'https://mocha2007.github.io/tools/ssev/earth_purple.jpg'
 			: 'https://upload.wikimedia.org/wikipedia/commons/2/2d/Meteosat-12-fci-march-equinox-2025-noon.jpg',
 			{albedo: t => CONSTANTS.glacial(t) ? 0.75 : 0.294, ghe: t => 500 < t ? 1.21
 			// ghe fit to https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/All_palaeotemps.svg/3840px-All_palaeotemps.svg.png
