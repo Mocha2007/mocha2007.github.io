347ab96cc2c0479372e789d3246b1fe56eedc8b5
Mocha <mocha2007@users.noreply.github.com>
2026-06-19 16:47:45 +0200

    restaurant

--- odd/syx.js
+++ odd/syx.js
@@ -25,7 +25,12 @@ class Species {
 		switch (species) {
 			case Species.AMEVIA: {
 				switch (profession) {
+					case Profession.FISHER:
+						return 1.25;
+					case Profession.FARMER_PASTURE_GLOBDIEN:
+						return 1.2;
 					case Profession.FARMER:
+					case Profession.FARMER_MUSHROOM:
 					case Profession.FARMER_PASTURE:
 					case Profession.ORCHARDIST:
 						return 0.8;
@@ -36,10 +41,12 @@ class Species {
 			case Species.CRETONIAN: {
 				switch (profession) {
 					case Profession.FARMER:
+					case Profession.FARMER_MUSHROOM:
 					case Profession.ORCHARDIST:
 						return 1.25;
-					case Profession.CHARCOALLER:
+					case Profession.BAKER:
 					case Profession.BREWER:
+					case Profession.CHARCOALLER:
 					case Profession.SMELTER:
 					case Profession.WEAVER:
 						return 1.1;
@@ -58,6 +65,7 @@ class Species {
 					case Profession.FARMER:
 						return 0.75; // excl. mushroom
 					case Profession.FARMER_PASTURE:
+					case Profession.FARMER_PASTURE_GLOBDIEN:
 						return 0.85;
 					case Profession.MINER:
 						return 1.15;
@@ -78,13 +86,16 @@ class Species {
 			case Species.GARTHIMI: {
 				switch (profession) {
 					case Profession.FARMER:
+					case Profession.FARMER_MUSHROOM:
 					case Profession.FARMER_PASTURE:
+					case Profession.FARMER_PASTURE_GLOBDIEN:
 					case Profession.ORCHARDIST:
 						return 0.75;
 					case Profession.MINER:
 						return 1.25;
-					case Profession.CHARCOALLER:
+					case Profession.BAKER:
 					case Profession.BREWER:
+					case Profession.CHARCOALLER:
 					case Profession.SMELTER:
 					case Profession.WEAVER:
 						return 0.9;
@@ -101,6 +112,7 @@ class Species {
 			case Species.HUMAN: {
 				switch (profession) {
 					case Profession.FARMER:
+					case Profession.FARMER_MUSHROOM:
 					case Profession.ORCHARDIST:
 						return 1.1;
 					default:
@@ -144,12 +156,17 @@ class Climate {
 }
 
 class Profession {
+	static BAKER = "baker";
 	static BREWER = "brewer";
 	static BOWYER = "bowyer";
 	static CARPENTER = "carpenter";
 	static CHARCOALLER = "charcoaller";
+	static CHEF = "chef";
 	static FARMER = "farmer";
+	static FARMER_MUSHROOM = "mushroom farmer";
 	static FARMER_PASTURE = "pasture farmer";
+	static FARMER_PASTURE_GLOBDIEN = "globdien farmer";
+	static FISHER = "fisher";
 	static ORCHARDIST = "orchardist";
 	static POTTER = "potter";
 	static MINER = "miner";
@@ -162,20 +179,27 @@ class Profession {
 
 class Item {
 	static ALCOHOL = "alcohol";
+	static BREAD = "bread";
 	static BOW = "bow";
 	static CLAY = "clay";
 	static CLOTHING = "clothing";
 	static COAL = "coal";
 	static COTTON = "cotton";
+	static EGG = "egg";
 	static FABRIC = "fabric";
 	static FALCATA = "falcata";
+	static FISH = "fish";
 	static FRUIT = "fruit";
 	static FURNITURE = "furniture";
 	static GRAIN = "grain";
+	static HERB = "herb";
 	static LEATHER = "leather";
+	static MEAT = "meat";
 	static METAL = "metal";
+	static MUSHROOM = "mushroom";
 	static ORE = "ore";
 	static POTTERY = "pottery";
+	static RESTAURANT = "restaurant";
 	static WOOD = "wood";
 	static cheapest_recipe(item, species){
 		const possible_recipes = this.recipes(item);
@@ -230,6 +254,8 @@ class Recipe {
 
 const DATA = {
 	recipes: [
+		new Recipe(Profession.BAKER, Item.BREAD, 6, [Item.GRAIN, Item.WOOD], [6, 1]),
+		new Recipe(Profession.BAKER, Item.BREAD, 7, [Item.GRAIN, Item.COAL], [7, 0.5]),
 		new Recipe(Profession.BREWER, Item.ALCOHOL, 2.25, [Item.FRUIT, Item.COAL, Item.POTTERY], [2, 0.5, 0.25]),
 		new Recipe(Profession.BREWER, Item.ALCOHOL, 2.5, [Item.GRAIN, Item.COAL, Item.POTTERY], [2, 0.5, 0.25]),
 		new Recipe(Profession.BOWYER, Item.BOW, 0.4, [Item.WOOD, Item.LEATHER], [4, 1]),
@@ -238,8 +264,15 @@ const DATA = {
 		new Recipe(Profession.FARMER, Item.COTTON, 3, [], [], [0.5, 1, 1.5]),
 		new Recipe(Profession.FARMER, Item.FRUIT, 2.1),
 		new Recipe(Profession.FARMER, Item.GRAIN, 4),
+		new Recipe(Profession.FARMER, Item.HERB, 0.25, [], [], [2, 1, 0.5]),
+		new Recipe(Profession.FARMER, Item.MUSHROOM, 1, [], [], [2, 1, 0.5]),
 		new Recipe(Profession.FARMER_PASTURE, Item.COTTON, 1.8, [], [], [1.25, 1, 0.75]),
 		new Recipe(Profession.FARMER_PASTURE, Item.LEATHER, 0.56, [], [], [0.75, 1.25, 0.75]),
+		// entelodont
+		new Recipe(Profession.FARMER_PASTURE, Item.MEAT, 1.12, [], [], [0.75, 1.1, 0.75]),
+		// globdien
+		new Recipe(Profession.FARMER_PASTURE_GLOBDIEN, Item.EGG, 1.05, [], [], [0.5, 1, 2]),
+		new Recipe(Profession.FISHER, Item.FISH, 1.4, [], [], [1.1, 1, 1]),
 		new Recipe(Profession.MINER, Item.CLAY, 1.5),
 		new Recipe(Profession.MINER, Item.COAL, 4),
 		new Recipe(Profession.MINER, Item.ORE, 1.5),
@@ -254,8 +287,17 @@ const DATA = {
 	],
 };
 
-// compute
-['alcohol', 'bow', 'clothing', 'falcata', 'furniture'].forEach(item_name => {
+// compute chef data
+[Item.BREAD, Item.EGG, Item.FISH, Item.FRUIT, Item.MEAT, Item.MUSHROOM].forEach(food => {
+	// chef logic (I think): 1 Herb + any food -> 1 meal???
+	// usable foods:
+	// fish bread fruit vegetables mushrooms meat eggs rations
+	DATA.recipes.push(new Recipe(Profession.CHEF, Item.RESTAURANT, 1, [Item.HERB, food], [1, 1]));
+});
+
+// compute table
+['alcohol', 'bow', 'clothing', 'falcata', 'furniture', 'restaurant']
+.forEach(item_name => {
 	/** @type {HTMLTableRowElement} */
 	const row_falcata = document.getElementById(item_name);
 	Species.species.forEach(species => {
